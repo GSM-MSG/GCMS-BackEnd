1856415155be70f76ac6d38b7c50ae9a5f5f7334
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-01-25 01:06:53 +0900

    :tada: 프로젝트 생성
    ㅇㅇ

---
+++ gcms/src/main/kotlin/com/msg/gcms/GcmsApplication.kt
@@ -0,0 +1,11 @@
+package com.msg.gcms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GcmsApplication
+
+fun main(args: Array<String>) {
+	runApplication<GcmsApplication>(*args)
+}
