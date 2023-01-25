ec13a28fd117351342722e56eb81894dd0dca127
Huuuunee <s21024@gsm.hs.kr>
2023-01-26 01:31:23 +0900

    :sparkles: 활동 이미지 엔티티 생성

---
+++ src/main/kotlin/com/msg/gcms/domain/club/domain/entity/ActivityImg.kt
@@ -0,0 +1,21 @@
+package com.msg.gcms.domain.club.domain.entity
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import javax.persistence.*
+
+@Table
+@Entity
+class ActivityImg(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+
+    @Column(columnDefinition="TEXT")
+    val Image: String,
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id")
+    val club: Club
+) {
+}
