446508ec1b7174a3744dfcc9c5968cb36b0c8775
jeayoung <k01066624566@gmail.com>
2023-01-25 20:38:54 +0900

    add redis properties

--- src/main/kotlin/com/msg/gcms/GcmsApplication.kt
+++ src/main/kotlin/com/msg/gcms/GcmsApplication.kt
@@ -1,9 +1,11 @@
 package com.msg.gcms
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class GcmsApplication
 
 fun main(args: Array<String>) {
