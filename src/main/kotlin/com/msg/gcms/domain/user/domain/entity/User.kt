2b509eaffcb9121d180924e93bdb9249b5af69fd
Huuuunee <s21024@gsm.hs.kr>
2023-01-26 01:29:49 +0900

    :sparkles: 유저 엔티티 생성

---
+++ src/main/kotlin/com/msg/gcms/domain/user/domain/entity/User.kt
@@ -0,0 +1,43 @@
+package com.msg.gcms.domain.user.domain.entity
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.msg.gcms.domain.club.domain.entity.Club
+import com.msg.gcms.domain.clubMember.domain.entity.ClubMember
+import com.msg.gcms.domain.joinStatus.domain.entity.JoinStatus
+import java.util.UUID
+import javax.persistence.*
+
+@Entity
+@Table
+class User(
+    @Id
+    var id: UUID,
+
+    @Column(length = 20)
+    val email: String,
+
+    @Column(length = 20)
+    val nickname: String,
+
+    val grade: Int,
+
+    val classNum: Int,
+
+    val number: Int,
+
+    @Column(columnDefinition="TEXT")
+    val profileImg: String,
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    val club: List<Club>?,
+
+    @JsonIgnore
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+    val joinStatus: List<JoinStatus>?,
+
+    @JsonIgnore
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+    val clubMember: List<ClubMember>?
+    ) {
+}
