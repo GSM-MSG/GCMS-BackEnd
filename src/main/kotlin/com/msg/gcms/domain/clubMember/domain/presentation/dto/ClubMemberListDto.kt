2d4d8c2904e77244dbe044596d3f265083891066
전승원 <78971821+louis7308@users.noreply.github.com>
2023-01-27 21:36:55 +0900

    :sparkles: :: ClubMemberListDto

---
+++ src/main/kotlin/com/msg/gcms/domain/clubMember/domain/presentation/dto/ClubMemberListDto.kt
@@ -0,0 +1,16 @@
+package com.msg.gcms.domain.clubMember.domain.presentation.dto
+
+import com.msg.gcms.domain.clubMember.enums.MemberScope
+import java.util.UUID
+
+class ClubMemberListDto(
+    private val uuid: UUID,
+    private val email: String,
+    private val name: String,
+    private val grade: Int,
+    private val classNum: Int,
+    private val number: Int,
+    private val profileImg: String,
+    private val scope: MemberScope
+) {
+}
