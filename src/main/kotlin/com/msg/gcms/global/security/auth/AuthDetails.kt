4a61d4b199fd547d55aad72b94286148d83f2d83
Huuuunee <s21024@gsm.hs.kr>
2023-01-28 06:16:40 +0900

    :sparkles: 유저 상세 서비스 생성

---
+++ src/main/kotlin/com/msg/gcms/global/security/auth/AuthDetails.kt
@@ -0,0 +1,23 @@
+package com.msg.gcms.global.security.auth
+
+import com.msg.gcms.domain.user.domain.entity.User
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class AuthDetails(
+    private val user: User
+) : UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority>? = null
+
+    override fun getPassword(): String? = null
+
+    override fun getUsername(): String = user.email
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
