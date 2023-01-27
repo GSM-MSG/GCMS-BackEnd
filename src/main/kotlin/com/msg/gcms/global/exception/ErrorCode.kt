b17821ae7520b9157cad9ca7efa7b9781ab574fe
jeayoung <k01066624566@gmail.com>
2023-01-27 22:48:48 +0900

    띄어쓰기 고침

--- src/main/kotlin/com/msg/gcms/global/exception/ErrorCode.kt
+++ src/main/kotlin/com/msg/gcms/global/exception/ErrorCode.kt
@@ -4,7 +4,7 @@ enum class ErrorCode(
     val message: String,
     val status: Int
 ) {
-    UNAUTHORIZED( "권한이 없음", 401),
+    UNAUTHORIZED("권한이 없음", 401),
 
     INTERNAL_SERVER_ERROR("서버 내부 에러", 500),
 }
