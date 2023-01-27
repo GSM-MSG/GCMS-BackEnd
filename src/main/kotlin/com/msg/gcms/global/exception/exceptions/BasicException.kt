aab3f14e384e2aa23ac41f6006c8a254b082455c
Huuuunee <s21024@gsm.hs.kr>
2023-01-28 06:10:10 +0900

    :recycle: open 클래스로 변경하였습니다

--- src/main/kotlin/com/msg/gcms/global/exception/exceptions/BasicException.kt
+++ src/main/kotlin/com/msg/gcms/global/exception/exceptions/BasicException.kt
@@ -2,4 +2,4 @@ package com.msg.gcms.global.exception.exceptions
 
 import com.msg.gcms.global.exception.ErrorCode
 
-class BasicException(val errorCode: ErrorCode): RuntimeException()
+open class BasicException(val errorCode: ErrorCode): RuntimeException()
