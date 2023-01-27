67f6e3c33236a90c7bf9c1fbc16ac6c531f43fd1
jeayoung <k01066624566@gmail.com>
2023-01-27 19:23:46 +0900

    지금 필요없는 객체 제거

--- src/main/kotlin/com/msg/gcms/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/msg/gcms/global/security/SecurityConfig.kt
@@ -1,10 +1,8 @@
 package com.msg.gcms.global.security
 
 import com.msg.gcms.global.security.filter.ExceptionFilter
-import com.msg.gcms.global.security.handler.CustomAccessDeniedHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
