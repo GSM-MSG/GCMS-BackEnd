4976e1e0898429ac58187ddb01ce7780417f23b9
Huuuunee <s21024@gsm.hs.kr>
2023-01-28 06:09:34 +0900

    :recycle: 유저아이디가 UUID이여서 UUID로 타입 변경

--- src/main/kotlin/com/msg/gcms/domain/auth/domain/entity/RefreshToken.kt
+++ src/main/kotlin/com/msg/gcms/domain/auth/domain/entity/RefreshToken.kt
@@ -3,12 +3,13 @@ package com.msg.gcms.domain.auth.domain.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.index.Indexed
+import java.util.UUID
 
 @RedisHash(value = "refreshToken", timeToLive = 60L * 60 * 24 * 7)
 class RefreshToken(
     @Id
     @Indexed
-    val userId: Long? = null,
+    val userId: UUID? = null,
     @Indexed
     val token: String,
 )
