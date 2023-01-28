6d2fadd2bb017058fcea60cfb8ec2e7bea183be1
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-01-28 17:04:28 +0900

    :sparkles: service 구현

---
+++ src/main/kotlin/com/msg/gcms/domain/club/domain/repository/ClubRepository.kt
@@ -0,0 +1,9 @@
+package com.msg.gcms.domain.club.domain.repository
+
+import com.msg.gcms.domain.club.domain.entity.Club
+import com.msg.gcms.domain.club.enums.ClubType
+import org.springframework.data.repository.CrudRepository
+
+interface ClubRepository: CrudRepository<Club, Long> {
+    fun findByType(type: ClubType): List<Club>
+}
--- src/main/kotlin/com/msg/gcms/domain/club/service/impl/FindClubListServiceImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/club/service/impl/FindClubListServiceImpl.kt
@@ -1,8 +1,18 @@
 package com.msg.gcms.domain.club.service.impl
 
+import com.msg.gcms.domain.club.domain.repository.ClubRepository
+import com.msg.gcms.domain.club.presentation.data.dto.ClubListDto
+import com.msg.gcms.domain.club.presentation.data.dto.ClubTypeDto
 import com.msg.gcms.domain.club.service.FindClubListService
+import com.msg.gcms.domain.club.utils.ClubConverter
 import org.springframework.stereotype.Service
 
 @Service
-class FindClubListServiceImpl: FindClubListService {
+class FindClubListServiceImpl(
+    private val clubRepository: ClubRepository,
+    private val clubConverter: ClubConverter
+): FindClubListService {
+    override fun execute(clubTypeDto: ClubTypeDto): List<ClubListDto> =
+        clubRepository.findByType(clubTypeDto.clubType)
+            .map { clubConverter.toDto(it) }
 }
