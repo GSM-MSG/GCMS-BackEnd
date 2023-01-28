e5226611c3aeb8ef1b7696847b31b1e4882ef3ed
전승원 <78971821+louis7308@users.noreply.github.com>
2023-01-29 01:04:56 +0900

    :sparkles: :: ClubMemberListResponse 를 반환할 Converter

--- src/main/kotlin/com/msg/gcms/domain/clubMember/domain/util/ClubMemberConverterImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/clubMember/domain/util/ClubMemberConverterImpl.kt
@@ -3,6 +3,7 @@ package com.msg.gcms.domain.clubMember.domain.util
 import com.msg.gcms.domain.clubMember.domain.entity.ClubMember
 import com.msg.gcms.domain.clubMember.domain.presentation.dto.ClubMemberDto
 import com.msg.gcms.domain.clubMember.domain.presentation.dto.ClubMemberListDto
+import com.msg.gcms.domain.clubMember.domain.presentation.dto.response.ClubMemberListResponse
 import com.msg.gcms.domain.clubMember.enums.MemberScope
 import org.springframework.stereotype.Component
 
@@ -22,5 +23,10 @@ class ClubMemberConverterImpl : ClubMemberConverter {
     override fun toListDto(scope: MemberScope, dto: List<ClubMemberDto>): ClubMemberListDto = ClubMemberListDto(
         scope = scope,
         clubMember = dto
+    )
+
+    override fun toResponse(dto: ClubMemberListDto): ClubMemberListResponse = ClubMemberListResponse(
+        scope = dto.scope,
+        clubMember = dto.clubMember
     )
 }
