505c041a6154c5825b5569079afedbed680439d1
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-01-28 14:54:15 +0900

    :sparkles: controller 생성
    ㅇㅇ

---
+++ src/main/kotlin/com/msg/gcms/domain/club/presentation/ClubController.kt
@@ -0,0 +1,9 @@
+package com.msg.gcms.domain.club.presentation
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/club")
+class ClubController {
+}
