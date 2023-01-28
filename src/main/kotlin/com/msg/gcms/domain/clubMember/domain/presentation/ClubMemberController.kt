94a6a9c3d10a1d147935ebfcf510013b29fbbd0e
전승원 <78971821+louis7308@users.noreply.github.com>
2023-01-29 01:09:22 +0900

    :sparkles: :: ClubMemberListDto를 Response로 변환 시켜주기

--- src/main/kotlin/com/msg/gcms/domain/clubMember/domain/presentation/ClubMemberController.kt
+++ src/main/kotlin/com/msg/gcms/domain/clubMember/domain/presentation/ClubMemberController.kt
@@ -1,16 +1,21 @@
 package com.msg.gcms.domain.clubMember.domain.presentation
 
+import com.msg.gcms.domain.clubMember.domain.presentation.dto.response.ClubMemberListResponse
 import com.msg.gcms.domain.clubMember.domain.service.FindClubMemberListService
+import com.msg.gcms.domain.clubMember.domain.util.ClubMemberConverter
+import com.msg.gcms.global.util.UserUtil
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/club-member")
 class ClubMemberController(
-    private val findClubMemberListService: FindClubMemberListService
+    private val findClubMemberListService: FindClubMemberListService,
+    private val clubMemberConverter: ClubMemberConverter
 ) {
 
     @GetMapping("{club-id}")
-    fun findAllByClubId(@PathVariable("club-id") clubId: Long) {
+    fun findAllByClubId(@PathVariable("club-id") clubId: Long): ClubMemberListResponse =
         findClubMemberListService.execute(clubId)
-    }
+            .let { clubMemberConverter.toResponse(it) }
+
 }
--- src/main/kotlin/com/msg/gcms/domain/clubMember/domain/service/FindClubMemberListService.kt
+++ src/main/kotlin/com/msg/gcms/domain/clubMember/domain/service/FindClubMemberListService.kt
@@ -4,31 +4,35 @@ import com.msg.gcms.domain.club.domain.entity.Club
 import com.msg.gcms.domain.club.domain.exception.ClubNotFoundException
 import com.msg.gcms.domain.club.domain.repository.ClubRepository
 import com.msg.gcms.domain.clubMember.domain.entity.ClubMember
+import com.msg.gcms.domain.clubMember.domain.exception.ClubMemberNonExistentException
 import com.msg.gcms.domain.clubMember.domain.presentation.dto.ClubMemberDto
+import com.msg.gcms.domain.clubMember.domain.presentation.dto.ClubMemberListDto
 import com.msg.gcms.domain.clubMember.domain.repository.ClubMemberRepository
 import com.msg.gcms.domain.clubMember.domain.util.ClubMemberConverter
 import com.msg.gcms.domain.clubMember.enums.MemberScope
 import com.msg.gcms.domain.user.domain.entity.User
+import com.msg.gcms.global.util.UserUtil
 import org.springframework.stereotype.Service
-import java.lang.RuntimeException
 
 @Service
 class FindClubMemberListService(
     private val clubMemberRepository: ClubMemberRepository,
     private val clubRepository: ClubRepository,
-    private val clubMemberConverter: ClubMemberConverter
+    private val clubMemberConverter: ClubMemberConverter,
+    private val userUtil: UserUtil
 ) {
 
-    fun execute(clubId: Long) {
+    fun execute(clubId: Long): ClubMemberListDto {
+        val user = userUtil.fetchCurrentUser()
         val club: Club = clubRepository.findById(clubId).orElseThrow { ClubNotFoundException() }
-        val clubMemberList: List<ClubMemberDto> = checkClubMemberScope()
+        val clubMemberList: List<ClubMemberDto> =
             clubMemberRepository.findAllByClub(club).map { clubMemberConverter.toDto(it) }
-        val scope: MemberScope = checkClubMemberScope()
-        return clubMemberConverter.toListDto()
+        val scope: MemberScope = checkClubMemberScope(user, club)
+        return clubMemberConverter.toListDto(scope, clubMemberList)
     }
 
     private fun checkClubMemberScope(user: User, club: Club): MemberScope {
-        val existsClub: ClubMember? = user.clubMember.find { it.club == club }
-        return existsClub?.scope ?: throw RuntimeException()
+        val existsClub: ClubMember? = club.clubMember.find { it.user == user }
+        return existsClub?.scope ?: throw ClubMemberNonExistentException()
     }
 }
