d35ca2a733949875ca0d2c31ffe17000df2d8013
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-01-28 16:09:55 +0900

    :sparkles: service 생성
    ㅇㅇ

---
+++ src/main/kotlin/com/msg/gcms/domain/club/service/FindClubListService.kt
@@ -0,0 +1,4 @@
+package com.msg.gcms.domain.club.service
+
+interface FindClubListService {
+}
---
+++ src/main/kotlin/com/msg/gcms/domain/club/service/impl/FindClubListServiceImpl.kt
@@ -0,0 +1,8 @@
+package com.msg.gcms.domain.club.service.impl
+
+import com.msg.gcms.domain.club.service.FindClubListService
+import org.springframework.stereotype.Service
+
+@Service
+class FindClubListServiceImpl: FindClubListService {
+}
