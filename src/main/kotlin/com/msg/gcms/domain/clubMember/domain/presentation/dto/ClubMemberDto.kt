ccbda3c65c1dd770d21297c8cb8764441f87918a
전승원 <78971821+louis7308@users.noreply.github.com>
2023-01-29 01:08:45 +0900

    :ambulance: :: private 처리를 public으로 변경

--- src/main/kotlin/com/msg/gcms/domain/clubMember/domain/presentation/dto/ClubMemberDto.kt
+++ src/main/kotlin/com/msg/gcms/domain/clubMember/domain/presentation/dto/ClubMemberDto.kt
@@ -4,15 +4,14 @@ import com.msg.gcms.domain.clubMember.enums.MemberScope
 import java.util.UUID
 
 class ClubMemberDto(
-    private val uuid: UUID,
-    private val email: String,
-    private val name: String,
-    private val grade: Int,
-    private val classNum: Int,
-    private val number: Int,
-    private val profileImg: String,
-    private val scope: MemberScope
+    val uuid: UUID,
+    val email: String,
+    val name: String,
+    val grade: Int,
+    val classNum: Int,
+    val number: Int,
     val profileImg: String?,
+    val scope: MemberScope
 ) {
 
 }
--- src/main/kotlin/com/msg/gcms/domain/clubMember/domain/presentation/dto/ClubMemberListDto.kt
+++ src/main/kotlin/com/msg/gcms/domain/clubMember/domain/presentation/dto/ClubMemberListDto.kt
@@ -3,7 +3,7 @@ package com.msg.gcms.domain.clubMember.domain.presentation.dto
 import com.msg.gcms.domain.clubMember.enums.MemberScope
 
 class ClubMemberListDto(
-    private val scope: MemberScope,
-    private val clubMember: List<ClubMemberDto>
+    val scope: MemberScope,
+    val clubMember: List<ClubMemberDto>
 ) {
 }
