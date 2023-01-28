25cab918434cb11af742d166e70ed3bcf425b299
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-01-28 16:53:33 +0900

    :sparkles: converter 구현

--- src/main/kotlin/com/msg/gcms/domain/club/utils/ClubConverter.kt
+++ src/main/kotlin/com/msg/gcms/domain/club/utils/ClubConverter.kt
@@ -1,4 +1,10 @@
 package com.msg.gcms.domain.club.utils
 
+import com.msg.gcms.domain.club.domain.entity.Club
+import com.msg.gcms.domain.club.presentation.data.dto.ClubListDto
+import com.msg.gcms.domain.club.presentation.data.dto.ClubTypeDto
+
 interface ClubConverter {
+    fun toDto(type: String): ClubTypeDto
+    fun toDto(club: Club): ClubListDto
 }
--- src/main/kotlin/com/msg/gcms/domain/club/utils/impl/ClubConverterImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/club/utils/impl/ClubConverterImpl.kt
@@ -1,8 +1,17 @@
 package com.msg.gcms.domain.club.utils.impl
 
+import com.msg.gcms.domain.club.domain.entity.Club
+import com.msg.gcms.domain.club.enums.ClubType
+import com.msg.gcms.domain.club.presentation.data.dto.ClubListDto
+import com.msg.gcms.domain.club.presentation.data.dto.ClubTypeDto
 import com.msg.gcms.domain.club.utils.ClubConverter
 import org.springframework.stereotype.Component
 
 @Component
 class ClubConverterImpl: ClubConverter {
+    override fun toDto(type: String): ClubTypeDto =
+        ClubTypeDto(clubType = ClubType.valueOf(type))
+
+    override fun toDto(club: Club): ClubListDto =
+        ClubListDto(id = club.id, type = club.type, title = club.name, bannerUrl = club.bannerImg )
 }
