8629ac313c7a32fd461c56341f500bcf21dfd741
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-01-28 16:04:08 +0900

    :sparkles: dto 생성
    ㅇㅇ

---
+++ src/main/kotlin/com/msg/gcms/domain/club/presentation/data/dto/ClubTypeDto.kt
@@ -0,0 +1,7 @@
+package com.msg.gcms.domain.club.presentation.data.dto
+
+import com.msg.gcms.domain.club.enums.ClubType
+
+data class ClubTypeDto(
+    val clubType: ClubType
+)
---
+++ src/main/kotlin/com/msg/gcms/domain/club/presentation/data/response/ClubListResponseDto.kt
@@ -0,0 +1,10 @@
+package com.msg.gcms.domain.club.presentation.data.response
+
+import com.msg.gcms.domain.club.enums.ClubType
+
+data class ClubListResponseDto(
+    val id: Long,
+    val type: ClubType,
+    val title: String,
+    val bannerUrl: String
+)
