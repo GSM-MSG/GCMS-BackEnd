196ccbd4cfa450bfbda0a98c38d3b3064c176328
전승원 <78971821+louis7308@users.noreply.github.com>
2023-01-29 17:02:14 +0900

    :ambulance: :: 띄어쓰기 해결

--- src/main/kotlin/com/msg/gcms/global/exception/ErrorCode.kt
+++ src/main/kotlin/com/msg/gcms/global/exception/ErrorCode.kt
@@ -9,7 +9,7 @@ enum class ErrorCode(
     EXPIRED_TOKEN("만료된 토큰", 401),
 
     USER_NOT_FOUND("해당 유저를 찾을 수 없음", 404),
-    CLUB_NOT_FOUND("동아리 를 찾을 수 없습니다.", 404),
+    CLUB_NOT_FOUND("동아리를 찾을 수 없습니다.", 404),
 
     INTERNAL_SERVER_ERROR("서버 내부 에러", 500),
 }
