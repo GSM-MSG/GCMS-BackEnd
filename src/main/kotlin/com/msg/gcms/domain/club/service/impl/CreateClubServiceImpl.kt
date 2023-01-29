052cd37c7e2c58c4ad005e628c2d7a921bd00119
jeayoung <k01066624566@gmail.com>
2023-01-29 20:02:21 +0900

    코드 스타일 일관되게 수정

--- src/main/kotlin/com/msg/gcms/domain/club/service/impl/CreateClubServiceImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/club/service/impl/CreateClubServiceImpl.kt
@@ -9,11 +9,13 @@ import com.msg.gcms.domain.club.utils.ClubConverter
 import com.msg.gcms.domain.clubMember.domain.entity.ClubMember
 import com.msg.gcms.domain.clubMember.domain.repository.ClubMemberRepository
 import com.msg.gcms.domain.clubMember.enums.MemberScope
+import com.msg.gcms.domain.user.domain.entity.User
 import com.msg.gcms.domain.user.domain.repository.UserRepository
 import com.msg.gcms.domain.user.exception.UserNotFoundException
 import com.msg.gcms.global.util.UserUtil
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.*
 
 @Service
 @Transactional(rollbackFor = [Exception::class])
@@ -30,18 +32,15 @@ class CreateClubServiceImpl(
         val club = clubConverter.toEntity(clubDto, currentUser)
         clubRepository.save(club)
         val activityImgs = clubDto.activityImgs
-            .map {
-                ActivityImg(image = it, club =  club)
-            }
+            .map { ActivityImg(image = it, club =  club) }
         activityImgRepository.saveAll(activityImgs)
         val users = clubDto.member
-            .map {
-                userRepository.findById(it)
-                    .orElseThrow { throw UserNotFoundException() }
-            }
-            .map {
-                ClubMember(scope = MemberScope.MEMBER, club = club, user = it)
-            }
+            .map { findById(it) }
+            .map { ClubMember(scope = MemberScope.MEMBER, club = club, user = it) }
         clubMemberRepository.saveAll(users)
     }
+
+    private fun findById(it: UUID): User =
+        userRepository.findById(it)
+            .orElseThrow { throw UserNotFoundException() }
 }
