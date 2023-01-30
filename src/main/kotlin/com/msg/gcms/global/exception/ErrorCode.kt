bd04858cd1a66a489e749e51acdeeb722e378454
전승원 <78971821+louis7308@users.noreply.github.com>
2023-01-30 11:08:52 +0900

    :ambulance: :: 줄 위치 변경

--- src/main/kotlin/com/msg/gcms/global/exception/ErrorCode.kt
+++ src/main/kotlin/com/msg/gcms/global/exception/ErrorCode.kt
@@ -4,9 +4,10 @@ enum class ErrorCode(
     val message: String,
     val status: Int
 ) {
-    CLUB_MEMBER_NON_EXISTENT("동아리 구성원이 아닌 경우", 403),
     UNAUTHORIZED("권한이 없음", 401),
     EXPIRED_TOKEN("만료된 토큰", 401),
+    
+    CLUB_MEMBER_NON_EXISTENT("동아리 구성원이 아닌 경우", 403),
 
     USER_NOT_FOUND("해당 유저를 찾을 수 없음", 404),
     CLUB_NOT_FOUND("동아리를 찾을 수 없습니다.", 404),
