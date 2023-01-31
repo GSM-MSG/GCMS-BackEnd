f969f3573718b5078926342d2c09181a478b31c6
전승원 <78971821+louis7308@users.noreply.github.com>
2023-01-31 23:40:15 +0900

    :ambulance: :: interface로 Service로직 구현 빼기

--- src/main/kotlin/com/msg/gcms/domain/clubMember/presentation/ClubMemberController.kt
+++ src/main/kotlin/com/msg/gcms/domain/clubMember/presentation/ClubMemberController.kt
@@ -2,6 +2,7 @@ package com.msg.gcms.domain.clubMember.presentation
 
 import com.msg.gcms.domain.clubMember.presentation.data.response.ClubMemberListResponse
 import com.msg.gcms.domain.clubMember.service.FindClubMemberListService
+import com.msg.gcms.domain.clubMember.service.impl.FindClubMemberListServiceImpl
 import com.msg.gcms.domain.clubMember.util.ClubMemberConverter
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
--- src/main/kotlin/com/msg/gcms/domain/clubMember/service/FindClubMemberListService.kt
+++ src/main/kotlin/com/msg/gcms/domain/clubMember/service/FindClubMemberListService.kt
@@ -1,42 +1,7 @@
 package com.msg.gcms.domain.clubMember.service
 
-import com.msg.gcms.domain.club.domain.entity.Club
-import com.msg.gcms.domain.club.domain.repository.ClubRepository
-import com.msg.gcms.domain.club.exception.ClubNotFoundException
-import com.msg.gcms.domain.clubMember.domain.entity.ClubMember
-import com.msg.gcms.domain.clubMember.domain.entity.enums.MemberScope
-import com.msg.gcms.domain.clubMember.domain.repository.ClubMemberRepository
-import com.msg.gcms.domain.clubMember.exception.ClubMemberNonExistentException
-import com.msg.gcms.domain.clubMember.presentation.data.dto.ClubMemberDto
 import com.msg.gcms.domain.clubMember.presentation.data.dto.ClubMemberListDto
-import com.msg.gcms.domain.clubMember.util.ClubMemberConverter
-import com.msg.gcms.domain.user.domain.entity.User
-import com.msg.gcms.global.util.UserUtil
-import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
-@Service
-@Transactional(readOnly = true)
-class FindClubMemberListService(
-    private val clubMemberRepository: ClubMemberRepository,
-    private val clubRepository: ClubRepository,
-    private val clubMemberConverter: ClubMemberConverter,
-    private val userUtil: UserUtil
-) {
-
-    fun execute(clubId: Long): ClubMemberListDto {
-        val user = userUtil.fetchCurrentUser()
-        val club: Club = clubRepository.findById(clubId)
-            .orElseThrow { ClubNotFoundException() }
-        val clubMemberList: List<ClubMemberDto> = clubMemberRepository.findAllByClub(club)
-            .map { clubMemberConverter.toDto(it) }
-        val scope: MemberScope = getScopeFromClubMember(user, club)
-        return clubMemberConverter.toListDto(scope, clubMemberList)
-    }
-
-    private fun getScopeFromClubMember(user: User, club: Club): MemberScope {
-        val existsClub: ClubMember? = club.clubMember
-            .find { it.user.id == user.id }
-        return existsClub?.scope ?: throw ClubMemberNonExistentException()
-    }
+interface FindClubMemberListService {
+    fun execute(clubId: Long): ClubMemberListDto
 }
