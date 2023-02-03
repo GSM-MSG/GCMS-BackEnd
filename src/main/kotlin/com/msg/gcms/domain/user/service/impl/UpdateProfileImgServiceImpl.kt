cb68707a21a64c61c522035cc4edc4796e319544
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-02-03 23:30:37 +0900

    :sparkles: 서비스 생성

---
+++ src/main/kotlin/com/msg/gcms/domain/user/service/impl/UpdateProfileImgServiceImpl.kt
@@ -0,0 +1,10 @@
+package com.msg.gcms.domain.user.service.impl
+
+import com.msg.gcms.domain.user.presentaion.data.dto.ProfileImgDto
+import com.msg.gcms.domain.user.service.UpdateProfileImgService
+
+class UpdateProfileImgServiceImpl : UpdateProfileImgService {
+    override fun execute(dto: ProfileImgDto) {
+        TODO("Not yet implemented")
+    }
+}
