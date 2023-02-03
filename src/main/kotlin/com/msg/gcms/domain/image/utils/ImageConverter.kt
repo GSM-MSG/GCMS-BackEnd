e688cab1cf959e117ac06e21ea7bcdef2db3be18
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-02-04 02:49:14 +0900

    :sparkles: converter 구현

--- src/main/kotlin/com/msg/gcms/domain/image/utils/ImageConverter.kt
+++ src/main/kotlin/com/msg/gcms/domain/image/utils/ImageConverter.kt
@@ -1,8 +1,10 @@
 package com.msg.gcms.domain.image.utils
 
 import com.msg.gcms.domain.image.presentation.data.dto.ImagesDto
+import com.msg.gcms.domain.image.presentation.data.response.ImagesResponseDto
 import org.springframework.web.multipart.MultipartFile
 
 interface ImageConverter {
-    fun toDto(file: List<MultipartFile>): ImagesDto
+    fun toDto(files: List<MultipartFile>): ImagesDto
+    fun toResponse(images: List<String>): ImagesResponseDto
 }
--- src/main/kotlin/com/msg/gcms/domain/image/utils/impl/ImageConverterImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/image/utils/impl/ImageConverterImpl.kt
@@ -1,12 +1,16 @@
 package com.msg.gcms.domain.image.utils.impl
 
 import com.msg.gcms.domain.image.presentation.data.dto.ImagesDto
+import com.msg.gcms.domain.image.presentation.data.response.ImagesResponseDto
 import com.msg.gcms.domain.image.utils.ImageConverter
 import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
 
 @Component
 class ImageConverterImpl : ImageConverter {
-    override fun toDto(file: List<MultipartFile>): ImagesDto =
-        ImagesDto(file)
+    override fun toDto(files: List<MultipartFile>): ImagesDto =
+        ImagesDto(images = files)
+
+    override fun toResponse(images: List<String>): ImagesResponseDto =
+        ImagesResponseDto(images = images)
 }
