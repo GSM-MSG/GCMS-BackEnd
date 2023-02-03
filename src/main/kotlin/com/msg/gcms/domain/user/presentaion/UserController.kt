4336665d7095198fa4e8a6f6064ca8106a24bc87
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-02-03 23:40:37 +0900

    :sparkles: 컨트롤러 구현

--- src/main/kotlin/com/msg/gcms/domain/user/presentaion/UserController.kt
+++ src/main/kotlin/com/msg/gcms/domain/user/presentaion/UserController.kt
@@ -1,13 +1,17 @@
 package com.msg.gcms.domain.user.presentaion
 
 import com.msg.gcms.domain.club.enums.ClubType
+import com.msg.gcms.domain.user.presentaion.data.request.UpdateProfileImgRequestDto
 import com.msg.gcms.domain.user.presentaion.data.response.SearchUserResponseDto
 import com.msg.gcms.domain.user.presentaion.data.response.UserResponseDto
 import com.msg.gcms.domain.user.service.FindUserService
 import com.msg.gcms.domain.user.service.SearchUserService
+import com.msg.gcms.domain.user.service.UpdateProfileImgService
 import com.msg.gcms.domain.user.utils.UserConverter
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -17,7 +21,8 @@ import org.springframework.web.bind.annotation.RestController
 class UserController(
     private val userConverter: UserConverter,
     private val findUserService: FindUserService,
-    private val searchUserService: SearchUserService
+    private val searchUserService: SearchUserService,
+    private val updateProfileImgService: UpdateProfileImgService
 ) {
     @GetMapping
     fun findUser(): ResponseEntity<UserResponseDto> {
@@ -34,4 +39,10 @@ class UserController(
             .let { searchUserService.execute(it) }
             .map { userConverter.toResponseDto(it) }
             .let { ResponseEntity.ok().body(it) }
+    @PatchMapping
+    fun updateProfileImg(@RequestBody requestDto: UpdateProfileImgRequestDto): ResponseEntity<Void> =
+        userConverter.toDto(requestDto)
+            .let { updateProfileImgService.execute(it) }
+            .let { ResponseEntity.noContent().build() }
+
 }
