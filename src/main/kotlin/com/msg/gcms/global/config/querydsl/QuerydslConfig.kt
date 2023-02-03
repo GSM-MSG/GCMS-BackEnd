07cbad7b05ae05d049413fd66d46c8b0d8aa7b94
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-02-03 20:20:56 +0900

    :sparkles: 어노테이션 및 import 삭제

--- src/main/kotlin/com/msg/gcms/global/config/querydsl/QuerydslConfig.kt
+++ src/main/kotlin/com/msg/gcms/global/config/querydsl/QuerydslConfig.kt
@@ -1,11 +1,12 @@
 package com.msg.gcms.global.config.querydsl
 
 import com.querydsl.jpa.impl.JPAQueryFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
 
+@Configuration
 class QuerydslConfig(
     @PersistenceContext
     val entityManager: EntityManager
