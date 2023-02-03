878c31ffd6e855f1e9e6f6fdd3a883979196108c
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-02-04 04:04:12 +0900

    :sparkles: 컨트롤러 테스트

---
+++ src/test/kotlin/com/msg/gcms/domain/image/controller/UploadImagesControllerTest.kt
@@ -0,0 +1,57 @@
+package com.msg.gcms.domain.image.controller
+
+import com.msg.gcms.domain.image.presentation.ImageController
+import com.msg.gcms.domain.image.presentation.data.dto.ImagesDto
+import com.msg.gcms.domain.image.presentation.data.dto.UploadImagesDto
+import com.msg.gcms.domain.image.presentation.data.response.ImagesResponseDto
+import com.msg.gcms.domain.image.service.UploadImageService
+import com.msg.gcms.domain.image.utils.ImageConverter
+import com.msg.gcms.domain.image.utils.impl.ImageConverterImpl
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpStatus
+import org.springframework.mock.web.MockMultipartFile
+import java.io.InputStream
+
+class UploadImagesControllerTest : BehaviorSpec({
+    @Bean
+    fun imageConverter(): ImageConverter {
+        return ImageConverterImpl()
+    }
+    val uploadImageService = mockk<UploadImageService>()
+    val imageController = ImageController(imageConverter(), uploadImageService)
+
+    given("upload image request") {
+        val size = (1..4).random()
+        val images = (1..size)
+            .map { MockMultipartFile("adfa.jpg", InputStream.nullInputStream()) }
+        val dto = UploadImagesDto(images)
+        val serviceResponseDto = ImagesDto(listOf())
+        val responseDto = ImagesResponseDto(listOf())
+
+        `when`("is received") {
+            every { uploadImageService.execute(dto) } returns serviceResponseDto
+            val response = imageController.uploadFile(images)
+            val body = response.body
+
+            then("response body should not be null") {
+                response.body shouldNotBe null
+            }
+
+            then("business logic in findUserService should be called") {
+                verify(exactly = 1) { uploadImageService.execute(dto) }
+            }
+            then("response status should be ok") {
+                response.statusCode shouldBe HttpStatus.OK
+            }
+            then("result should be same as responseDto") {
+                body shouldBe responseDto
+            }
+        }
+    }
+})
