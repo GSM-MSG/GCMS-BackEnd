b4360d3bb247f9db29e66c10b44b9102c1ca4a69
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-02-04 02:47:51 +0900

    :recycle: 반환값 수정

--- src/main/kotlin/com/msg/gcms/domain/image/presentation/data/response/ImagesResponseDto.kt
+++ src/main/kotlin/com/msg/gcms/domain/image/presentation/data/response/ImagesResponseDto.kt
@@ -1,7 +1,6 @@
 package com.msg.gcms.domain.image.presentation.data.response
 
-import org.springframework.web.multipart.MultipartFile
 
 data class ImagesResponseDto(
-    val images: List<MultipartFile>
+    val images: List<String>
 )
