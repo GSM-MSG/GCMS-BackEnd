57a063ef400ee3bfe09ce478a863736eff75b9d5
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-02-04 21:07:43 +0900

    :sparkles: 컨트롤러 구현

--- src/main/kotlin/com/msg/gcms/domain/user/service/impl/WithdrawUserServiceImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/user/service/impl/WithdrawUserServiceImpl.kt
@@ -1,7 +1,9 @@
 package com.msg.gcms.domain.user.service.impl
 
 import com.msg.gcms.domain.user.service.WithdrawUserService
+import org.springframework.stereotype.Service
 
+@Service
 class WithdrawUserServiceImpl : WithdrawUserService {
     override fun execute() {
         TODO("Not yet implemented")
