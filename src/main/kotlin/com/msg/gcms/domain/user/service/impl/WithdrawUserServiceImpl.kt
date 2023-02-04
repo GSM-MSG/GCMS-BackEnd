4c93a93df1c5a2932268799f1027974759437af4
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-02-04 20:58:09 +0900

    :sparkles: 서비스 생성

---
+++ src/main/kotlin/com/msg/gcms/domain/user/service/impl/WithdrawUserServiceImpl.kt
@@ -0,0 +1,9 @@
+package com.msg.gcms.domain.user.service.impl
+
+import com.msg.gcms.domain.user.service.WithdrawUserService
+
+class WithdrawUserServiceImpl : WithdrawUserService {
+    override fun execute() {
+        TODO("Not yet implemented")
+    }
+}
