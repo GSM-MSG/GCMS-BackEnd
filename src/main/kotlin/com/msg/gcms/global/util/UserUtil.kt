ed46fc322dedc2742be29ef783d593e0bb4d55ed
전승원 <78971821+louis7308@users.noreply.github.com>
2023-02-04 21:52:53 +0900

    :ambulance: :: 어이쿠 실수

--- src/main/kotlin/com/msg/gcms/global/util/UserUtil.kt
+++ src/main/kotlin/com/msg/gcms/global/util/UserUtil.kt
@@ -22,5 +22,5 @@ class UserUtil(
     }
 
     fun fetchUserByEmail(email: String): User =
-        userRepository.findByEmail("jswa7308@naver.com") ?: throw UserNotFoundException()
+        userRepository.findByEmail(email) ?: throw UserNotFoundException()
 }
