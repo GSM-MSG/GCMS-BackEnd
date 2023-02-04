a228f4480c28b271db51624ac89174c390067a0f
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-02-04 22:38:23 +0900

    :sparkles: cade 옵션

--- src/main/kotlin/com/msg/gcms/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/msg/gcms/global/security/SecurityConfig.kt
@@ -49,6 +49,7 @@ class SecurityConfig(
             .antMatchers(HttpMethod.GET,"/user").authenticated()
             .antMatchers(HttpMethod.GET,"/user/search").authenticated()
             .antMatchers(HttpMethod.PATCH, "/user").authenticated()
+            .antMatchers(HttpMethod.DELETE, "/user").permitAll()
 
             .anyRequest().denyAll()
             .and()
