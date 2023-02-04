9e233de085e2672141643f8e3675e5db66728102
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-02-04 21:50:37 +0900

    :sparkles: 컨트롤러 테스트코드 구현

---
+++ src/test/kotlin/com/msg/gcms/domain/user/controller/WithdrawUserControllerTest.kt
@@ -0,0 +1,38 @@
+package com.msg.gcms.domain.user.controller
+
+import com.msg.gcms.domain.user.presentaion.UserController
+import com.msg.gcms.domain.user.service.FindUserService
+import com.msg.gcms.domain.user.service.SearchUserService
+import com.msg.gcms.domain.user.service.UpdateProfileImgService
+import com.msg.gcms.domain.user.service.WithdrawUserService
+import com.msg.gcms.domain.user.utils.UserConverter
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.springframework.http.HttpStatus
+
+class WithdrawUserControllerTest : BehaviorSpec({
+    val userConverter = mockk<UserConverter>()
+    val searchUserService = mockk<SearchUserService>()
+    val findUserService = mockk<FindUserService>()
+    val updateProfileImgService = mockk<UpdateProfileImgService>()
+    val withdrawUserService = mockk<WithdrawUserService>()
+    val clubController = UserController(userConverter, findUserService, searchUserService, updateProfileImgService, withdrawUserService)
+
+    given("withdraw user request") {
+
+        `when`("is received") {
+            every { withdrawUserService.execute() } returns Unit
+            val response = clubController.withdrawUser()
+
+            then("business logic in withdrawUserService should be called") {
+                verify(exactly = 1) { withdrawUserService.execute() }
+            }
+            then("response status should be no_content") {
+                response.statusCode shouldBe HttpStatus.NO_CONTENT
+            }
+        }
+    }
+})
