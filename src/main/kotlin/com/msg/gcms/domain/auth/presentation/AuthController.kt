5a22c2a147091a14bb348209cd30d0a8ab8cb98c
Huuuunee <s21024@gsm.hs.kr>
2023-02-11 02:40:05 +0900

    :sparkles: 리프레시 토큰 재발급 컨트롤러 매핑

--- src/main/kotlin/com/msg/gcms/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/com/msg/gcms/domain/auth/presentation/AuthController.kt
@@ -1,25 +1,29 @@
 package com.msg.gcms.domain.auth.presentation
 
 import com.msg.gcms.domain.auth.presentation.data.request.SignInRequestDto
+import com.msg.gcms.domain.auth.presentation.data.response.NewRefreshTokenResponseDto
 import com.msg.gcms.domain.auth.presentation.data.response.SignInResponseDto
+import com.msg.gcms.domain.auth.service.GetNewRefreshTokenService
 import com.msg.gcms.domain.auth.service.SignInService
 import com.msg.gcms.domain.auth.util.AuthConverter
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/auth")
 class AuthController(
     private val authConverter: AuthConverter,
-    private val signInService: SignInService
+    private val signInService: SignInService,
+    private val getNewRefreshTokenService: GetNewRefreshTokenService
 ) {
 
     @PostMapping
     fun signIn(@RequestBody signInRequestDto: SignInRequestDto): ResponseEntity<SignInResponseDto> =
         authConverter.toDto(signInRequestDto)
             .let { ResponseEntity.ok(signInService.execute(it)) }
+
+    @PatchMapping
+    fun getNewRefreshToken(@RequestHeader("Refresh-Token") refreshToken: String): ResponseEntity<NewRefreshTokenResponseDto> =
+        ResponseEntity.ok(getNewRefreshTokenService.execute(refreshToken))
 
 }
