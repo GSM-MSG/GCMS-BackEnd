579044580b725c750dd5956fa9505fcae357a37c
Huuuunee <s21024@gsm.hs.kr>
2023-02-11 17:02:02 +0900

    :recycle: 중괄호 삭제

--- src/main/kotlin/com/msg/gcms/domain/auth/exception/ExpiredRefreshTokenException.kt
+++ src/main/kotlin/com/msg/gcms/domain/auth/exception/ExpiredRefreshTokenException.kt
@@ -3,5 +3,4 @@ package com.msg.gcms.domain.auth.exception
 import com.msg.gcms.global.exception.ErrorCode
 import com.msg.gcms.global.exception.exceptions.BasicException
 
-class ExpiredRefreshTokenException : BasicException(ErrorCode.EXPIRED_TOKEN){
-}
+class ExpiredRefreshTokenException : BasicException(ErrorCode.EXPIRED_TOKEN)
