a4cbe526cf1053f1fda18489b8fcf799c6d3b24c
Huuuunee <s21024@gsm.hs.kr>
2023-02-11 17:09:39 +0900

    :recycle: 파일명 gAuth -> gauth로 변경

--- src/main/kotlin/com/msg/gcms/domain/auth/service/impl/SignInServiceImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/auth/service/impl/SignInServiceImpl.kt
@@ -6,7 +6,7 @@ import com.msg.gcms.domain.auth.service.SignInService
 import com.msg.gcms.domain.auth.util.AuthConverter
 import com.msg.gcms.domain.auth.util.AuthUtil
 import com.msg.gcms.domain.user.domain.repository.UserRepository
-import com.msg.gcms.global.gAuth.properties.GAuthProperties
+import com.msg.gcms.global.gauth.properties.GAuthProperties
 import com.msg.gcms.global.security.jwt.JwtTokenProvider
 import gauth.GAuth
 import gauth.GAuthToken
