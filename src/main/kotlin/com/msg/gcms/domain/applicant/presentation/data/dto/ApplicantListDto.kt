63b2d1ee979bbf3e32d14113ca6f41e6e7035168
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-02-13 17:29:24 +0900

    :sparkles: dto 구현

--- src/main/kotlin/com/msg/gcms/domain/applicant/presentation/data/dto/ApplicantListDto.kt
+++ src/main/kotlin/com/msg/gcms/domain/applicant/presentation/data/dto/ApplicantListDto.kt
@@ -1,4 +1,19 @@
 package com.msg.gcms.domain.applicant.presentation.data.dto
 
-class ApplicantListDto {
+import com.msg.gcms.domain.club.enums.Scope
+import java.util.UUID
+
+data class ApplicantListDto (
+    val scope: Scope,
+    val applicantList: List<ApplicantDto>
+) {
+    data class ApplicantDto (
+        val uuid: UUID,
+        val email: String,
+        val name: String,
+        val grade: Int,
+        val classNum: Int,
+        val number: Int,
+        val profileImg: String?
+    )
 }
