de211d0cb6f2ad5188de9dbd67f451f37ef0ae3f
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-02-15 21:51:44 +0900

    :sparkles: 컨버터 구현

--- src/main/kotlin/com/msg/gcms/domain/user/utils/UserConverter.kt
+++ src/main/kotlin/com/msg/gcms/domain/user/utils/UserConverter.kt
@@ -3,15 +3,14 @@ package com.msg.gcms.domain.user.utils
 import com.msg.gcms.domain.club.domain.entity.Club
 import com.msg.gcms.domain.club.enums.ClubType
 import com.msg.gcms.domain.user.domain.entity.User
-import com.msg.gcms.domain.user.presentaion.data.dto.ProfileImgDto
-import com.msg.gcms.domain.user.presentaion.data.dto.SearchRequirementDto
-import com.msg.gcms.domain.user.presentaion.data.dto.SearchUserDto
-import com.msg.gcms.domain.user.presentaion.data.dto.UserDto
+import com.msg.gcms.domain.user.presentaion.data.dto.*
 import com.msg.gcms.domain.user.presentaion.data.request.UpdateProfileImgRequestDto
 import com.msg.gcms.domain.user.presentaion.data.response.SearchUserResponseDto
+import com.msg.gcms.domain.user.presentaion.data.response.UserProfileResponseDto
 import com.msg.gcms.domain.user.presentaion.data.response.UserResponseDto
 
 interface UserConverter {
+    fun toUserProfileDto(user: User): UserProfileDto
     fun toDto(user: User, dto: List<UserDto.ClubDto>): UserDto
     fun toDto(club: Club): UserDto.ClubDto
     fun toDto(type: ClubType, name: String): SearchRequirementDto
@@ -19,5 +18,6 @@ interface UserConverter {
     fun toDto(requestDto: UpdateProfileImgRequestDto): ProfileImgDto
     fun toResponseDto(dto: SearchUserDto): SearchUserResponseDto
     fun toResponseDto(dto: UserDto.ClubDto): UserResponseDto.ClubResponseDto
+    fun toResponseDto(dto: UserProfileDto): UserProfileResponseDto
     fun toResponseDto(dto: UserDto, responseDto: List<UserResponseDto.ClubResponseDto>): UserResponseDto
 }
