7161f7905032cb2fe12d0eaef7bce9c864e2f4b5
baegteun <baegteun@gmail.com>
2023-02-20 22:35:42 +0900

    Update WebMvcConfig.kt

--- src/main/kotlin/com/msg/gcms/global/web/WebMvcConfig.kt
+++ src/main/kotlin/com/msg/gcms/global/web/WebMvcConfig.kt
@@ -6,11 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebMvcConfig: WebMvcConfigurer {
-  override fun addCorsMappings(registry: CorsRegistry) {
-    registry.addMapping("/**")
-      .allowedOrigins("https://gcms-front-end-v2.vercel.app")
-      .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "HEAD")
-      .allowedHeaders("Authorization", "Refresh-Token")
-      .allowCredentials(true)
-  }
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://gcms-front-end-v2.vercel.app")
+            .allowedMethods("*")
+            .allowedHeaders("Authorization", "Refresh-Token")
+            .allowCredentials(true)
+    }
 }
