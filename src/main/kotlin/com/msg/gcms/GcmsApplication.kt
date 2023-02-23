6038d48da809144f353bbe3879353c0881212265
Huuuunee <s21024@gsm.hs.kr>
2023-02-23 16:47:47 +0900

    :recycle: 수정사항

--- src/main/kotlin/com/msg/gcms/GcmsApplication.kt
+++ src/main/kotlin/com/msg/gcms/GcmsApplication.kt
@@ -3,11 +3,13 @@ package com.msg.gcms
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 class GcmsApplication
 
 fun main(args: Array<String>) {
+	TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
 	runApplication<GcmsApplication>(*args)
 }
