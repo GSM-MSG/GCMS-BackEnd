fa143484ddbff5e7266fc68bb3562e282fca11b2
전승원 <78971821+louis7308@users.noreply.github.com>
2023-02-28 19:01:35 +0900

    :sparkles: :: admin 토큰 생성 및 권한 부여 로직 구현

--- src/main/kotlin/com/msg/gcms/domain/auth/service/impl/SignInServiceImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/auth/service/impl/SignInServiceImpl.kt
@@ -1,5 +1,7 @@
 package com.msg.gcms.domain.auth.service.impl
 
+import com.msg.gcms.domain.admin.domain.repository.AdminRepository
+import com.msg.gcms.domain.auth.domain.Role
 import com.msg.gcms.domain.auth.presentation.data.dto.SignInDto
 import com.msg.gcms.domain.auth.presentation.data.response.SignInResponseDto
 import com.msg.gcms.domain.auth.service.SignInService
@@ -22,7 +24,8 @@ class SignInServiceImpl(
     private val authConverter: AuthConverter,
     private val jwtTokenProvider: JwtTokenProvider,
     private val gAuth: GAuth,
-    private val authUtil: AuthUtil
+    private val authUtil: AuthUtil,
+    private val adminRepository: AdminRepository
 ) : SignInService {
 
 
@@ -34,18 +37,17 @@ class SignInServiceImpl(
             gAuthProperties.redirectUri
         )
         val gAuthUserInfo: GAuthUserInfo = gAuth.getUserInfo(gAuthToken.accessToken)
+        val role = getRoleByGauthInfo(gAuthUserInfo.role)
 
-        val userInfo = userRepository.findByEmail(gAuthUserInfo.email)
-
-        val accessToken: String = jwtTokenProvider.generateAccessToken(gAuthUserInfo.email)
-        val refreshToken: String = jwtTokenProvider.generateRefreshToken(gAuthUserInfo.email)
+        val accessToken: String = jwtTokenProvider.generateAccessToken(gAuthUserInfo.email, role)
+        val refreshToken: String = jwtTokenProvider.generateRefreshToken(gAuthUserInfo.email, role)
         val accessExp: ZonedDateTime = jwtTokenProvider.accessExpiredTime
         val refreshExp: ZonedDateTime = jwtTokenProvider.refreshExpiredTime
 
-        if (userInfo == null) {
-            authUtil.saveNewUser(gAuthUserInfo, refreshToken)
+        if(role == Role.ADMIN) {
+            createAdminOrRefreshToken(gAuthUserInfo, refreshToken)
         } else {
-            authUtil.saveNewRefreshToken(userInfo, refreshToken)
+            createUserOrRefreshToken(gAuthUserInfo, refreshToken)
         }
 
         return SignInResponseDto(
@@ -54,6 +56,32 @@ class SignInServiceImpl(
             accessExp = accessExp,
             refreshExp = refreshExp
         )
+    }
+
+    private fun getRoleByGauthInfo(role: String): Role {
+        return when (role) {
+            "ROLE_STUDENT" -> Role.USER
+            "ROLE_TEACHER" -> Role.ADMIN
+            else -> throw TODO("존재하지 않는 role 입니다.")
+        }
+    }
+
+    private fun createUserOrRefreshToken(gAuthUserInfo: GAuthUserInfo, refreshToken: String) {
+        val userInfo = userRepository.findByEmail(gAuthUserInfo.email)
+        if (userInfo == null) {
+            authUtil.saveNewUser(gAuthUserInfo, refreshToken)
+        } else {
+            authUtil.saveNewRefreshToken(userInfo, refreshToken)
+        }
+    }
+
+    private fun createAdminOrRefreshToken(gAuthUserInfo: GAuthUserInfo, refreshToken: String) {
+        val adminInfo = adminRepository.findByEmail(gAuthUserInfo.email)
+        if (adminInfo == null) {
+            authUtil.saveNewAdmin(gAuthUserInfo, refreshToken)
+        } else {
+            authUtil.saveNewRefreshToken(adminInfo, refreshToken)
+        }
     }
 
 }
--- src/main/kotlin/com/msg/gcms/global/security/jwt/JwtTokenProvider.kt
+++ src/main/kotlin/com/msg/gcms/global/security/jwt/JwtTokenProvider.kt
@@ -1,5 +1,7 @@
 package com.msg.gcms.global.security.jwt
 
+import com.msg.gcms.domain.auth.domain.Role
+import com.msg.gcms.global.security.auth.AdminDetailsService
 import com.msg.gcms.global.security.auth.AuthDetailsService
 import com.msg.gcms.global.security.exception.ExpiredTokenException
 import com.msg.gcms.global.security.exception.InvalidTokenException
@@ -10,6 +12,7 @@ import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
 import java.security.Key
 import java.time.ZonedDateTime
@@ -19,7 +22,8 @@ import javax.servlet.http.HttpServletRequest
 @Component
 class JwtTokenProvider(
     private val jwtProperties: JwtProperties,
-    private val authDetailsService: AuthDetailsService
+    private val authDetailsService: AuthDetailsService,
+    private val adminDetailsService: AdminDetailsService
 ) {
     companion object {
         const val ACCESS_TYPE = "access"
@@ -27,6 +31,7 @@ class JwtTokenProvider(
         const val ACCESS_EXP = 60L * 15 // 15 min
         const val REFRESH_EXP = 60L * 60 * 24 * 7 // 1 weeks
         const val TOKEN_PREFIX = "Bearer "
+        const val AUTHORITY = "authority"
     }
 
     val accessExpiredTime: ZonedDateTime
@@ -35,11 +40,11 @@ class JwtTokenProvider(
     val refreshExpiredTime: ZonedDateTime
         get() = ZonedDateTime.now().plusSeconds(REFRESH_EXP)
 
-    fun generateAccessToken(email: String): String =
-        generateToken(email, ACCESS_TYPE, jwtProperties.accessSecret, ACCESS_EXP)
+    fun generateAccessToken(email: String, role: Role): String =
+        generateToken(email, ACCESS_TYPE, jwtProperties.accessSecret, ACCESS_EXP, role)
 
-    fun generateRefreshToken(email: String): String =
-        generateToken(email, REFRESH_TYPE, jwtProperties.refreshSecret, REFRESH_EXP)
+    fun generateRefreshToken(email: String, role: Role): String =
+        generateToken(email, REFRESH_TYPE, jwtProperties.refreshSecret, REFRESH_EXP, role)
 
     fun resolveToken(req: HttpServletRequest): String? {
         val token = req.getHeader("Authorization") ?: return null
@@ -50,23 +55,34 @@ class JwtTokenProvider(
         return getTokenSubject(refresh, jwtProperties.refreshSecret)
     }
 
+    fun exactRoleFromRefreshToken(refresh: String): Role {
+        return when (getTokenBody(refresh, jwtProperties.refreshSecret).get(AUTHORITY, String::class.java)) {
+            "USER" -> Role.USER
+            "ADMIN" -> Role.ADMIN
+            else -> TODO("올바르지 않은 권한 입니다.")
+        }
+
+    }
+
     fun exactTypeFromRefreshToken(refresh: String): String =
         getTokenSubject(refresh, jwtProperties.refreshSecret)
 
     fun authentication(token: String): Authentication {
-        val userDetails = authDetailsService.loadUserByUsername(getTokenSubject(token, jwtProperties.accessSecret))
+        val userDetails = getLoadByUserDetail(token)
         return UsernamePasswordAuthenticationToken(userDetails, "", userDetails.authorities)
     }
 
     fun parseToken(token: String): String? =
         if (token.startsWith(TOKEN_PREFIX)) token.replace(TOKEN_PREFIX, "") else null
 
-    fun generateToken(email: String, type: String, secret: Key, exp: Long): String {
+    fun generateToken(email: String, type: String, secret: Key, exp: Long, role: Role): String {
+        val claims = Jwts.claims().setSubject(email)
+        claims["type"] = type
+        claims[AUTHORITY] = role
         return Jwts.builder()
             .setHeaderParam("typ", "JWT")
             .signWith(secret, SignatureAlgorithm.HS256)
-            .setSubject(email)
-            .claim("type", type)
+            .setClaims(claims)
             .setIssuedAt(Date())
             .setExpiration(Date(System.currentTimeMillis() + exp * 1000))
             .compact()
@@ -88,4 +104,12 @@ class JwtTokenProvider(
 
     private fun getTokenSubject(token: String, secret: Key): String =
         getTokenBody(token, secret).subject
+
+    private fun getLoadByUserDetail(token: String): UserDetails {
+        return when (getTokenBody(token, jwtProperties.accessSecret).get(AUTHORITY, String::class.java)) {
+            Role.USER.name -> authDetailsService.loadUserByUsername(getTokenSubject(token, jwtProperties.accessSecret))
+            Role.ADMIN.name -> adminDetailsService.loadUserByUsername(getTokenSubject(token, jwtProperties.accessSecret))
+            else -> throw TODO("유효하지 않은 토큰")
+        }
+    }
 }
