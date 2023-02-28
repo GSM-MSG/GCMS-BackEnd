b4c4e990af4546c5b7267a8dffa98fa3e12a5365
전승원 <78971821+louis7308@users.noreply.github.com>
2023-02-28 18:59:45 +0900

    :sparkles: :: AdminDetails 추가

--- src/main/kotlin/com/msg/gcms/global/security/auth/AdminDetails.kt
+++ src/main/kotlin/com/msg/gcms/global/security/auth/AdminDetails.kt
@@ -1,19 +1,19 @@
 package com.msg.gcms.global.security.auth
 
+import com.msg.gcms.domain.admin.domain.entity.Admin
 import com.msg.gcms.domain.auth.domain.Role
-import com.msg.gcms.domain.user.domain.entity.User
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
-class AuthDetails(
-    private val user: User
+class AdminDetails(
+    private val admin: Admin
 ) : UserDetails {
-    override fun getAuthorities(): MutableCollection<out GrantedAuthority>? = mutableListOf(SimpleGrantedAuthority(Role.USER.name))
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = mutableListOf(SimpleGrantedAuthority(Role.ADMIN.name))
 
     override fun getPassword(): String? = null
 
-    override fun getUsername(): String = user.email
+    override fun getUsername(): String = admin.id.toString()
 
     override fun isAccountNonExpired(): Boolean = true
 
