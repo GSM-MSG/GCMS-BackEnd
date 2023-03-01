c23528f906ea52202970490d417e062eb06b35aa
jeayoung <k01066624566@gmail.com>
2023-03-02 07:35:38 +0900

    안쓰는 예외제거

--- src/main/kotlin/com/msg/gcms/domain/admin/exception/AdminNotFoundException.kt
+++
@@ -1,7 +0,0 @@
-package com.msg.gcms.domain.admin.exception
-
-import com.msg.gcms.global.exception.ErrorCode
-import com.msg.gcms.global.exception.exceptions.BasicException
-
-class AdminNotFoundException : BasicException(ErrorCode.ADMIN_NOT_FOUND) {
-}
--- src/main/kotlin/com/msg/gcms/domain/auth/exception/AuthorityNotExistException.kt
+++
@@ -1,7 +0,0 @@
-package com.msg.gcms.domain.auth.exception
-
-import com.msg.gcms.global.exception.ErrorCode
-import com.msg.gcms.global.exception.exceptions.BasicException
-
-class AuthorityNotExistException : BasicException(ErrorCode.AUTHORITY_NOT_EXIST) {
-}
