dec6a346fae94ec125884c2ac2b30b8021362cc6
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-06 02:13:03 +0900

    :recycle: provider 코드 수정

--- src/main/kotlin/com/msg/gcms/global/security/jwt/properties/TokenTimeProperties.kt
+++ src/main/kotlin/com/msg/gcms/global/security/jwt/properties/TokenTimeProperties.kt
@@ -6,6 +6,6 @@ import org.springframework.boot.context.properties.ConstructorBinding
 @ConstructorBinding
 @ConfigurationProperties(prefix = "jwt.time")
 class TokenTimeProperties (
-     accessTime: Long,
-     refreshTime: Long
+    val accessTime: Long,
+    val refreshTime: Long
 )
