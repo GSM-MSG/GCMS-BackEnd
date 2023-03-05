e5ace0ffc4edc36bd9ddd93529247ee82fd94a83
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-05 16:58:32 +0900

    :sparkles: 서비스 구현

--- src/main/kotlin/com/msg/gcms/domain/admin/service/impl/RejectClubServiceImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/admin/service/impl/RejectClubServiceImpl.kt
@@ -1,11 +1,21 @@
 package com.msg.gcms.domain.admin.service.impl
 
 import com.msg.gcms.domain.admin.service.RejectClubService
+import com.msg.gcms.domain.club.domain.repository.ClubRepository
+import com.msg.gcms.domain.club.enums.ClubStatus
+import com.msg.gcms.domain.club.exception.ClubNotFoundException
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class RejectClubServiceImpl : RejectClubService {
+@Transactional(rollbackFor = [Exception::class])
+class RejectClubServiceImpl(
+    private val clubRepository: ClubRepository
+) : RejectClubService {
     override fun execute(clubId: Long) {
-        TODO("Not yet implemented")
+        val club = clubRepository.findByIdOrNull(clubId) ?: throw ClubNotFoundException()
+
+        clubRepository.delete(club)
     }
 }
