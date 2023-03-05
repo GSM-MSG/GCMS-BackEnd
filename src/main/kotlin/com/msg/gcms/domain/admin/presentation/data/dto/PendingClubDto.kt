51261a26eecf3311d05617c3f0ccd901c0a64278
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-06 00:40:16 +0900

    :recycle: clubId값 추가

--- src/main/kotlin/com/msg/gcms/domain/admin/presentation/data/dto/PendingClubDto.kt
+++ src/main/kotlin/com/msg/gcms/domain/admin/presentation/data/dto/PendingClubDto.kt
@@ -3,6 +3,7 @@ package com.msg.gcms.domain.admin.presentation.data.dto
 import com.msg.gcms.domain.club.enums.ClubType
 
 data class PendingClubDto (
+    val id: Long,
     val bannerImg: String,
     val name: String,
     val type: ClubType,
