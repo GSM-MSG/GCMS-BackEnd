9a2c315f6a99a42185ff1983d78559184e6f523d
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-05 17:33:01 +0900

    :sparkles: 컨트롤러 구현

--- src/main/kotlin/com/msg/gcms/domain/admin/presentation/AdminController.kt
+++ src/main/kotlin/com/msg/gcms/domain/admin/presentation/AdminController.kt
@@ -1,8 +1,10 @@
 package com.msg.gcms.domain.admin.presentation
 
+import com.msg.gcms.domain.admin.presentation.data.response.PendingClubResponse
 import com.msg.gcms.domain.admin.service.AcceptClubService
 import com.msg.gcms.domain.admin.service.CreateClubMemberExcelByClassNumService
 import com.msg.gcms.domain.admin.service.CreateClubMemberExcelService
+import com.msg.gcms.domain.admin.service.FindPendingClubListService
 import com.msg.gcms.domain.admin.util.AdminConverter
 import com.msg.gcms.domain.club.enums.ClubType
 import com.msg.gcms.global.annotation.RequestController
@@ -19,6 +21,7 @@ class AdminController(
     private val createClubMemberExcelService: CreateClubMemberExcelService,
     private val createClubMemberExcelByClassNumService: CreateClubMemberExcelByClassNumService,
     private val adminConverter: AdminConverter,
+    private val findPendingClubListService: FindPendingClubListService
 ) {
 
     @GetMapping("/excel/club")
@@ -32,6 +35,12 @@ class AdminController(
         response.setHeader("Content-Disposition", "attachment; filename=club_assign_status.xlsx")
         return createClubMemberExcelByClassNumService.execute(clubType)
     }
+
+    @GetMapping
+    fun findPendingClub(): ResponseEntity<List<PendingClubResponse>> =
+        findPendingClubListService.execute()
+            .map { adminConverter.toResponse(it) }
+            .let { ResponseEntity.ok().body(it) }
 
     @PatchMapping("/{club_id}")
     fun acceptClub(@PathVariable("club_id") clubId: Long) : ResponseEntity<Void> =
