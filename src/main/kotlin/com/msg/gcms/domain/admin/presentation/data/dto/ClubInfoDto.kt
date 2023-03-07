a57cc662c00f4b92443d0e4cfb6ddbfc662a39e9
louis7308 <jswa7308@naver.com>
2023-03-07 09:00:14 +0900

    admin user 상세 정보 데이터 값 변경

--- src/main/kotlin/com/msg/gcms/domain/admin/presentation/data/dto/ClubInfoDto.kt
+++ src/main/kotlin/com/msg/gcms/domain/admin/presentation/data/dto/ClubInfoDto.kt
@@ -1,6 +1,10 @@
 package com.msg.gcms.domain.admin.presentation.data.dto
 
+import com.msg.gcms.domain.club.enums.ClubType
+
 data class ClubInfoDto(
+        val id: Long,
         val bannerImg: String?,
         val name: String?,
+        val type: ClubType
 )
