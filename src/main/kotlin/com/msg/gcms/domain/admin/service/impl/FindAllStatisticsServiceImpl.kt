637ab9338e8427ea641f49aa27ad1b3caf03bf00
전승원 <78971821+louis7308@users.noreply.github.com>
2023-03-08 17:08:17 +0900

    :sparkless: :: 통계 데이터 조회 로직

---
+++ src/main/kotlin/com/msg/gcms/domain/admin/service/impl/FindAllStatisticsServiceImpl.kt
@@ -0,0 +1,39 @@
+package com.msg.gcms.domain.admin.service.impl
+
+import com.msg.gcms.domain.admin.presentation.data.dto.FindAllStatisticsDto
+import com.msg.gcms.domain.admin.service.FindAllStatisticsService
+import com.msg.gcms.domain.admin.util.AdminConverter
+import com.msg.gcms.domain.auth.domain.Role
+import com.msg.gcms.domain.club.domain.entity.Club
+import com.msg.gcms.domain.club.domain.repository.ClubRepository
+import com.msg.gcms.domain.club.enums.ClubType
+import com.msg.gcms.domain.clubMember.domain.repository.ClubMemberRepository
+import com.msg.gcms.domain.user.domain.entity.User
+import com.msg.gcms.domain.user.domain.repository.UserRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true, rollbackFor = [Exception::class])
+class FindAllStatisticsServiceImpl(
+    private val clubRepository: ClubRepository,
+    private val clubMemberRepository: ClubMemberRepository,
+    private val userRepository: UserRepository,
+    private val adminConverter: AdminConverter
+) : FindAllStatisticsService {
+    override fun execute(clubType: ClubType): FindAllStatisticsDto {
+        var applicantCount: Int = 0
+        val userTotalCount: Int = userRepository.findAllByRoles(Role.ROLE_STUDENT).count()
+        clubRepository.findByType(clubType)
+            .forEach { applicantCount += getApplicantCount(it) }
+        return adminConverter.toDto(1, applicantCount)
+    }
+
+    private fun getApplicantCount(club: Club): Int {
+        var clubHeadCount = 0
+        if(club.user != null)
+            clubHeadCount = 1
+        val clubMemberCount = club.clubMember.count()
+        return clubHeadCount + clubMemberCount
+    }
+}
--- src/main/kotlin/com/msg/gcms/domain/user/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/msg/gcms/domain/user/domain/repository/UserRepository.kt
@@ -1,5 +1,6 @@
 package com.msg.gcms.domain.user.domain.repository
 
+import com.msg.gcms.domain.auth.domain.Role
 import com.msg.gcms.domain.club.enums.ClubType
 import com.msg.gcms.domain.user.domain.entity.User
 import org.springframework.data.jpa.repository.Query
@@ -10,4 +11,5 @@ import java.util.UUID
 interface UserRepository: CrudRepository<User, UUID>, UserRepositoryCustom {
     fun findByEmail(email: String) : User?
     fun findByNicknameContaining(name: String): List<User>
+    fun findAllByRoles(role: Role): List<User>
 }
