097b7ee727b3e6befea2aef6a943dd49fab4ffae
jeayoung <k01066624566@gmail.com>
2023-03-09 14:05:30 +0900

    푸시 알림보내는 코드작성

--- src/main/kotlin/com/msg/gcms/domain/user/domain/entity/User.kt
+++ src/main/kotlin/com/msg/gcms/domain/user/domain/entity/User.kt
@@ -48,8 +48,6 @@ class User(
     @JsonIgnore
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
     val clubMember: List<ClubMember> = listOf(),
-
-    val fcmToken: String = "",
 ) {
     fun updateProfileImg(profileImg: String?) {
         this.profileImg = profileImg
--- src/main/kotlin/com/msg/gcms/global/util/MessageSendUtil.kt
+++ src/main/kotlin/com/msg/gcms/global/util/MessageSendUtil.kt
@@ -1,8 +1,10 @@
 package com.msg.gcms.global.util
 
+import com.msg.gcms.domain.user.domain.entity.User
 import com.msg.gcms.global.fcm.enums.SendType
 import java.util.*
 
 interface MessageSendUtil {
-    fun sendMessage(token: String, title: String, content: String, type: SendType, identify: UUID?)
+    fun sendMessage(token: String, title: String, content: String, type: SendType)
+    fun send(user: User, title: String, content: String, type: SendType)
 }
