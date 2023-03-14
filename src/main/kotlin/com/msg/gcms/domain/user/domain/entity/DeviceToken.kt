cf7714048acdc98886e1526347a3408c7254f203
전승원 <78971821+louis7308@users.noreply.github.com>
2023-03-14 14:35:50 +0900

    :sparkles: :: DevieceToken 에 OnDelete 옵션 없어서 추가

--- src/main/kotlin/com/msg/gcms/domain/user/domain/entity/DeviceToken.kt
+++ src/main/kotlin/com/msg/gcms/domain/user/domain/entity/DeviceToken.kt
@@ -1,5 +1,7 @@
 package com.msg.gcms.domain.user.domain.entity
 
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
 import java.util.UUID
 import javax.persistence.*
 
@@ -11,6 +13,7 @@ class DeviceToken (
 
     @MapsId
     @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "user_id", columnDefinition = "BINARY(16)")
     val user: User,
 
