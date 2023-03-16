20fcf70dbad27f676d73187e0b88aabf33907651
baegteun <baegteun@gmail.com>
2023-03-16 23:43:18 +0900

    Update UserUtil.kt

--- src/main/kotlin/com/msg/gcms/global/util/UserUtil.kt
+++ src/main/kotlin/com/msg/gcms/global/util/UserUtil.kt
@@ -21,6 +21,8 @@ class UserUtil(
         userRepository.findByEmail(email) ?: throw UserNotFoundException()
     fun fetchUserEmail(): String {
         val principal = SecurityContextHolder.getContext().authentication.principal
+        println(principal)
+        println(principal is UserDetails)
         val email = if (principal is UserDetails) {
             (principal as AuthDetails).username
         } else {
