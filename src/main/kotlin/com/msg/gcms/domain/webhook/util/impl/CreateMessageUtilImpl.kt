84bbd77a962f0e4d0964e5b75325cf0b347ef6c2
ani2689 <ojno7777@gmail.com>
2023-04-26 13:54:53 +0900

    :sparkles: 메세지 유틸 구현

---
+++ src/main/kotlin/com/msg/gcms/domain/webhook/util/impl/CreateMessageUtilImpl.kt
@@ -0,0 +1,38 @@
+package com.msg.gcms.domain.webhook.util.impl
+
+import com.msg.gcms.domain.webhook.util.CreateMessageUtil
+import org.springframework.stereotype.Service
+
+@Service
+class CreateMessageUtilImpl : CreateMessageUtil {
+
+    override fun execute(club_name: String, club_type: String, club_img: String) = """
+        {
+          "content": null,
+          "embeds": [
+            {
+              "title": "띵동!",
+              "description": "새로운 동아리 신설 요청이 들어왔어요.",
+              "color": 4097741,
+              "fields": [
+                {
+                  "name": "동아리 이름",
+                  "value": "$club_name",
+                  "inline": true
+                },
+                {
+                  "name": "동아리 유형",
+                  "value": "$club_type",
+                  "inline": true
+                }
+              ],
+              "image": {
+                "url": "$club_img"
+              }
+            }
+          ],
+          "username": "띵동이",
+          "attachments": []
+        }
+        """.trimIndent()
+}
