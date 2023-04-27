3b91cfc6c1df1a609716632e50db53b5158bcb7b
ani2689 <ojno7777@gmail.com>
2023-04-27 11:58:45 +0900

    :fire: Util 통합

--- src/main/kotlin/com/msg/gcms/domain/webhook/util/CreateMessageUtil.kt
+++
@@ -1,7 +0,0 @@
-package com.msg.gcms.domain.webhook.util
-
-import com.msg.gcms.domain.club.enums.ClubType
-
-interface CreateMessageUtil {
-    fun execute(clubName: String, clubType: ClubType, clubImg: String): String
-}
