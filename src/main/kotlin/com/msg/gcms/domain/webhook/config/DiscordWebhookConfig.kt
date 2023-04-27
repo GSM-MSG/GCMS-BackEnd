71f29511dbc829a35eed6cc64de939bd9c659858
ani2689 <ojno7777@gmail.com>
2023-04-27 16:15:11 +0900

    :fire: 디스코드 빈 주입 삭제

--- src/main/kotlin/com/msg/gcms/domain/webhook/config/DiscordWebhookConfig.kt
+++ src/main/kotlin/com/msg/gcms/domain/webhook/config/DiscordWebhookConfig.kt
@@ -1,20 +1,13 @@
 package com.msg.gcms.domain.webhook.config
 
 import okhttp3.OkHttpClient
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class DiscordWebhookConfig (
-    @Value("\${discord.webhook.url}")
-    private val discordWebhookUrl: String
-){
+class DiscordWebhookConfig{
 
     private val httpClient = OkHttpClient()
-
-    @Bean
-    fun discordWebhookUrl() = discordWebhookUrl
 
     @Bean
     fun httpClient() = httpClient
