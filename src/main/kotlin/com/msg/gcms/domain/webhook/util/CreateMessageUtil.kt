f5fcd3aff20f767ec3a9b86d5665305d71671e5a
노현주 <106813267+ani2689@users.noreply.github.com>
2023-04-27 09:20:26 +0900

    :recycle: lowerCamlCase로 수정
    Co-authored-by: baegteun <baegteun@gmail.com>

--- src/main/kotlin/com/msg/gcms/domain/webhook/util/CreateMessageUtil.kt
+++ src/main/kotlin/com/msg/gcms/domain/webhook/util/CreateMessageUtil.kt
@@ -3,5 +3,5 @@ package com.msg.gcms.domain.webhook.util
 import com.msg.gcms.domain.club.enums.ClubType
 
 interface CreateMessageUtil {
-    fun execute(club_name: String, club_type: ClubType, club_img: String): String
+    fun execute(clubName: String, clubType: ClubType, clubImg: String): String
 }
