ff5020e2c558e3b6517b9d69bad2496538b25d1b
ani2689 <ojno7777@gmail.com>
2023-05-28 02:43:57 +0900

    :bug: /n -> //n 으로 수정

--- src/main/kotlin/com/msg/gcms/global/logger/LoggingScheduler.kt
+++ src/main/kotlin/com/msg/gcms/global/logger/LoggingScheduler.kt
@@ -39,9 +39,9 @@ class LoggingScheduler(
                 file.delete()
                 logUrlList.add(url+fileName)
             }
-        val message = StringBuilder("**로그 목록**\n")
+        val message = StringBuilder("**로그 목록**\\n")
         logUrlList.forEachIndexed { idx, str ->
-            message.append("${idx + 1} $str\n")
+            message.append("${idx + 1} $str\\n")
         }
         discordUtil.sendDiscordMessage(message.toString())
     }
