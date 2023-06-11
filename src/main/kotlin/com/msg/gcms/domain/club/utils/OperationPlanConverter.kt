1953b92320abd894cad58b913cbf56a59ca03fad
KimTaeO <taeo0328@gmail.com>
2023-06-12 08:09:17 +0900

    :bug: :: 잠결에 저지른 일 수습

--- src/main/kotlin/com/msg/gcms/domain/club/utils/OperationPlanConverter.kt
+++ src/main/kotlin/com/msg/gcms/domain/club/utils/OperationPlanConverter.kt
@@ -8,22 +8,5 @@ import com.msg.gcms.domain.club.presentation.data.request.CreateOperationPlanReq
 interface OperationPlanConverter {
     fun toDto(createOperationPlanRequest: CreateOperationPlanRequest): OperationPlanDto
     fun toDto(monthlyPlan: List<CreateOperationPlanRequest.CreateMonthlyPlanRequest>): List<OperationPlanDto.MonthlyPlanDto>
-    fun toEntity(clubId: Long, operationPlan: OperationPlan, club: Club): Club =
-        Club(
-            id = clubId,
-            name = club.name,
-            bannerImg = club.bannerImg,
-            content = club.content,
-            notionLink = club.notionLink,
-            teacher = club.teacher,
-            contact = club.contact,
-            type = club.type,
-            isOpened = club.isOpened,
-            user = club.user,
-            activityImg = club.activityImg,
-            applicant = club.applicant,
-            clubMember = club.clubMember,
-            clubStatus = club.clubStatus,
-            operationPlan = operationPlan
-        )
+    fun toEntity(clubId: Long, operationPlan: OperationPlan, club: Club): Club
 }
--- src/main/kotlin/com/msg/gcms/domain/club/utils/impl/OperationPlanConverterImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/club/utils/impl/OperationPlanConverterImpl.kt
@@ -1,5 +1,7 @@
 package com.msg.gcms.domain.club.utils.impl
 
+import com.msg.gcms.domain.club.domain.entity.Club
+import com.msg.gcms.domain.club.domain.entity.OperationPlan
 import com.msg.gcms.domain.club.presentation.data.dto.OperationPlanDto
 import com.msg.gcms.domain.club.presentation.data.request.CreateOperationPlanRequest
 import com.msg.gcms.domain.club.utils.OperationPlanConverter
@@ -19,4 +21,23 @@ class OperationPlanConverterImpl: OperationPlanConverter {
 
     override fun toDto(monthlyPlan: List<CreateOperationPlanRequest.CreateMonthlyPlanRequest>): List<OperationPlanDto.MonthlyPlanDto> =
         monthlyPlan.map { OperationPlanDto.MonthlyPlanDto(it) }
+
+    override fun toEntity(clubId: Long, operationPlan: OperationPlan, club: Club): Club =
+        Club(
+            id = clubId,
+            name = club.name,
+            bannerImg = club.bannerImg,
+            content = club.content,
+            notionLink = club.notionLink,
+            teacher = club.teacher,
+            contact = club.contact,
+            type = club.type,
+            isOpened = club.isOpened,
+            user = club.user,
+            activityImg = club.activityImg,
+            applicant = club.applicant,
+            clubMember = club.clubMember,
+            clubStatus = club.clubStatus,
+            operationPlan = operationPlan
+        )
 }
