ecbef09ecc9a692e141f4f237222ce028abee633
KimTaeO <taeo0328@gmail.com>
2023-06-14 01:34:14 +0900

    :sparkles: :: 운영계획서 파일 반환 api Controller 작성

--- src/main/kotlin/com/msg/gcms/domain/admin/presentation/AdminController.kt
+++ src/main/kotlin/com/msg/gcms/domain/admin/presentation/AdminController.kt
@@ -28,7 +28,8 @@ class AdminController(
     private val rejectClubService: RejectClubService,
     private val findAllUserListService: FindAllUserListService,
     private val userDetailInfoService: UserDetailInfoService,
-    private val findAllStatisticsService: FindAllStatisticsService
+    private val findAllStatisticsService: FindAllStatisticsService,
+    private val clubOperationPlanHwpService: ClubOperationPlanHwpService
 ) {
 
     @GetMapping("/excel/club")
@@ -84,5 +85,8 @@ class AdminController(
             .let { ResponseEntity.ok().body(it) }
 
     @GetMapping("/hwp/operation/{club_id}")
-    fun 
+    fun getOperationPlanToHwp(@PathVariable("club_id") clubId: Long, response: HttpServletResponse): ByteArray {
+        response.setHeader("Content-Disposition", "attachment; filename=club.xlsx")
+        return clubOperationPlanHwpService.execute(clubId)
+    }
 }
