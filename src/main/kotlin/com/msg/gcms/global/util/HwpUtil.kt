8a5f7b0439e27f2870533a90b9fa06111f4665d2
KimTaeO <taeo0328@gmail.com>
2023-06-13 14:30:59 +0900

    :sparkles: :: 한글 파일을 다루는데 필요한 기능들 Util로 분리

---
+++ src/main/kotlin/com/msg/gcms/global/util/HwpUtil.kt
@@ -0,0 +1,9 @@
+package com.msg.gcms.global.util
+
+import kr.dogfoot.hwplib.`object`.HWPFile
+
+interface HwpUtil {
+    fun readFile(url: String): HWPFile
+
+    fun findCellByFieldName(hwpFile: HWPFile, index: Int, fieldName: String, content: String)
+}
---
+++ src/main/kotlin/com/msg/gcms/global/util/impl/HwpUtilImpl.kt
@@ -0,0 +1,23 @@
+package com.msg.gcms.global.util.impl
+
+import com.msg.gcms.global.util.HwpUtil
+import kr.dogfoot.hwplib.`object`.HWPFile
+import kr.dogfoot.hwplib.`object`.bodytext.paragraph.text.ParaText
+import kr.dogfoot.hwplib.reader.HWPReader
+import kr.dogfoot.hwplib.tool.objectfinder.CellFinder
+import org.springframework.stereotype.Component
+
+@Component
+class HwpUtilImpl: HwpUtil {
+    override fun readFile(url: String): HWPFile =
+        HWPReader.fromURL(url)
+
+    override fun findCellByFieldName(hwpFile: HWPFile, index: Int, fieldName: String, content: String) {
+        CellFinder.findAll(hwpFile, fieldName)
+            .map {
+                val para = it.paragraphList.getParagraph(index)
+                para.text ?: para.createText()
+                para.text.addString(content)
+            }
+    }
+}
