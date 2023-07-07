d99bba03a315287f3be115b960704d1ad9f4657a
KimTaeO <taeo0328@gmail.com>
2023-07-07 14:37:29 +0900

    :sparkles: CustomAccessDeniedHandler 작성

---
+++ src/main/kotlin/com/msg/gcms/global/security/CustomAccessDeniedHandler.kt
@@ -0,0 +1,34 @@
+package com.msg.gcms.global.security
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.msg.gcms.global.exception.ErrorCode
+import com.msg.gcms.global.exception.ErrorResponse
+import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.web.access.AccessDeniedHandler
+import org.springframework.stereotype.Component
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class CustomAccessDeniedHandler(
+    private val objectMapper: ObjectMapper
+): AccessDeniedHandler {
+
+    private val logger = LoggerFactory.getLogger(this.javaClass.simpleName)
+
+    override fun handle(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        accessDeniedException: AccessDeniedException?
+    ) {
+        logger.warn("==========Access Denied==========")
+        val errorCode = ErrorCode.FORBIDDEN
+        val responseString = objectMapper.writeValueAsString(ErrorResponse(errorCode.message, errorCode.status))
+        response.characterEncoding = "UTF-8"
+        response.status = errorCode.status
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.writer.write(responseString)
+    }
+}
