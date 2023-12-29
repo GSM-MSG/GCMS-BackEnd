00d26f8107717210fccf493a8019a222e49eb0b9
KimTaeO <taeo0328@gmail.com>
2023-12-29 12:08:33 +0900

    refactor :: service에서 properties를 의존하지 않도록 리팩토링

--- src/main/kotlin/com/msg/gcms/global/util/HwpUtil.kt
+++ src/main/kotlin/com/msg/gcms/global/util/HwpUtil.kt
@@ -3,7 +3,8 @@ package com.msg.gcms.global.util
 import kr.dogfoot.hwplib.`object`.HWPFile
 
 interface HwpUtil {
-    fun readFile(url: String): HWPFile
+    fun readOperationPlan(): HWPFile
+    fun readOpeningApplication(): HWPFile
 
     fun insertByFieldName(hwpFile: HWPFile, fieldName: String, content: String?)
 }
