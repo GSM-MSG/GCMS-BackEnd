c13e01ad83a39ebc40e72756e4c9f2eaa3160be8
KimTaeO <taeo0328@gmail.com>
2023-12-30 17:04:15 +0900

    add :: 개설신청서 누락된 필드 추가

--- src/main/kotlin/com/msg/gcms/domain/club/domain/entity/OpeningApplication.kt
+++ src/main/kotlin/com/msg/gcms/domain/club/domain/entity/OpeningApplication.kt
@@ -11,11 +11,18 @@ class OpeningApplication(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
 
+    // 연구 분야
+    val field: String,
+
+    //연구 주제
     val subject: String,
 
+    // 연구 분야 및 주제 선정 이유
     val reason: String,
 
+    // 연구 활동 목표
     val target: String,
 
+    // 기대 효과
     val effect: String
 )
