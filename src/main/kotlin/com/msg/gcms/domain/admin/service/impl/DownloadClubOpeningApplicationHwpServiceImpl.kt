bdc77506cc75cc9e21d0e136ef818a91e0b1f5e1
KimTaeO <taeo0328@gmail.com>
2024-01-06 13:16:00 +0900

    refactor :: 보일러 플레이트 코드 개선

--- src/main/kotlin/com/msg/gcms/domain/admin/service/impl/DownloadClubOpeningApplicationHwpServiceImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/admin/service/impl/DownloadClubOpeningApplicationHwpServiceImpl.kt
@@ -6,6 +6,7 @@ import com.msg.gcms.domain.club.enums.ClubStatus
 import com.msg.gcms.domain.club.exception.ClubNotFoundException
 import com.msg.gcms.domain.user.domain.entity.User
 import com.msg.gcms.global.util.HwpUtil
+import com.msg.gcms.main
 import kr.dogfoot.hwplib.writer.HWPWriter
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
@@ -30,34 +31,33 @@ class DownloadClubOpeningApplicationHwpServiceImpl(
 
         val clubOwnerInfo = getStudentInfo(clubOwner)
 
-        hwpUtil.insertByFieldName(openingApplicationForm, "신청인 정보", "학번 및 성명 : $clubOwnerInfo ${clubOwner.nickname}")
-
         val (new, maintain) =
             if(club.clubStatus == ClubStatus.PENDING)
                     ("신설[ v ]" to "유지[  ]")
             else
                     ("신설[  ]" to "유지[ v ]")
 
-        hwpUtil.insertByFieldName(openingApplicationForm, "신청 형태 신설", new)
-        hwpUtil.insertByFieldName(openingApplicationForm, "신청 형태 유지", maintain)
-
-        hwpUtil.insertByFieldName(openingApplicationForm, "동아리명", club.name)
-        hwpUtil.insertByFieldName(openingApplicationForm, "연구 분야", openingApplication.field)
-        hwpUtil.insertByFieldName(openingApplicationForm, "연구 주제", openingApplication.subject)
-        hwpUtil.insertByFieldName(openingApplicationForm, "선정 이유", openingApplication.reason)
-        hwpUtil.insertByFieldName(openingApplicationForm, "연구 활동 목표", openingApplication.subject)
-
-        hwpUtil.insertByFieldName(openingApplicationForm, "학번1", clubOwnerInfo)
-        hwpUtil.insertByFieldName(openingApplicationForm, "이름1", clubOwner.nickname)
-        hwpUtil.insertByFieldName(openingApplicationForm, "역할1", "부장")
-
-        for (i in 2..club.clubMember.size) {
-            hwpUtil.insertByFieldName(openingApplicationForm, "학번$i", getStudentInfo(club.clubMember[i].user))
-            hwpUtil.insertByFieldName(openingApplicationForm, "이름$i", club.clubMember[i].user.nickname)
-            hwpUtil.insertByFieldName(openingApplicationForm, "역할$i", "")
+        mapOf(
+            Pair("신청인 정보", "학번 및 성명 : $clubOwnerInfo ${clubOwner.nickname}"),
+            Pair("신청 형태 신설", new),
+            Pair("신청 형태 유지", maintain),
+            Pair("동아리명", club.name),
+            Pair("연구 분야", openingApplication.field),
+            Pair("연구 주제", openingApplication.subject),
+            Pair("선정 이유", openingApplication.reason),
+            Pair("연구 활동 목표", openingApplication.subject),
+            Pair("학번1", clubOwnerInfo),
+            Pair("이름1", clubOwner.nickname),
+            Pair("역할1", "부장"),
+            Pair("기대 효과", openingApplication.effect),
+            *(2..club.clubMember.size).map {
+                    Pair("학번$it", getStudentInfo(club.clubMember[it - 1].user))
+                    Pair("이름$it", club.clubMember[it - 1].user.nickname)
+                    Pair("역할$it", "")
+            }.toTypedArray()
+        ).forEach {
+            hwpUtil.insertByFieldName(openingApplicationForm, it.key, it.value)
         }
-
-        hwpUtil.insertByFieldName(openingApplicationForm, "기대 효과", openingApplication.effect)
 
         val outPutStream = ByteArrayOutputStream()
 
