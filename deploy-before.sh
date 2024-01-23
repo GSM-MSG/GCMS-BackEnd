40ee3cac65eaee5753d83ed42b53144f201b1a39
shk0625 <tjfgml1238@gmail.com>
2024-01-23 22:08:21 +0900

    update ::  deploy 오타 수정

--- deploy-before.sh
+++ deploy-before.sh
@@ -1,4 +1,4 @@
-if [ -d /home/ec2-user/build ]; then
-    sudo rm -rf /home/ec2-user/build/
+if [ -d /home/ec2-user/deploy ]; then
+    sudo rm -rf /home/ec2-user/deploy/
 fi
 sudo mkdir -vp /home/ec2-user/deploy/
