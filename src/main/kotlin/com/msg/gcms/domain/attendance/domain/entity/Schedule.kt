692a78f19904298f7bb5b3c540de6d7be6547f5f
KimTaeO <taeo0328@gmail.com>
2024-03-12 21:06:05 +0900

    create :: schedule entity

---
+++ src/main/kotlin/com/msg/gcms/domain/attendance/domain/entity/Schedule.kt
@@ -0,0 +1,24 @@
+package com.msg.gcms.domain.attendance.domain.entity
+
+import com.msg.gcms.domain.club.domain.entity.Club
+import java.time.LocalDate
+import javax.persistence.*
+
+@Entity
+class Schedule(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @Column(columnDefinition = "VARCHAR(30)", name = "name")
+    val name: String,
+
+    @Column(columnDefinition = "TIME", name = "date", updatable = false)
+    val date: LocalDate,
+
+    @Column(columnDefinition = "VARCHAR(20)", name = "period")
+    val period: String,
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    val club: Club
+)
