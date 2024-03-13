ddeeec00d546b22518f7da1f74195448337c2b10
KimTaeO <taeo0328@gmail.com>
2024-03-14 01:52:19 +0900

    add :: custom query schedule repository

--- src/main/kotlin/com/msg/gcms/domain/attendance/repository/ScheduleRepository.kt
+++ src/main/kotlin/com/msg/gcms/domain/attendance/repository/ScheduleRepository.kt
@@ -3,4 +3,4 @@ package com.msg.gcms.domain.attendance.repository
 import com.msg.gcms.domain.attendance.domain.entity.Schedule
 import org.springframework.data.repository.CrudRepository
 
-interface ScheduleRepository : CrudRepository<Schedule, Long>
+interface ScheduleRepository : CrudRepository<Schedule, Long>, CustomScheduleRepository
