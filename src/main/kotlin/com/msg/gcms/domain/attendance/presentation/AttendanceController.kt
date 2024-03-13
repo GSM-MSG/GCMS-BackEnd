c31aadaa480264a63bd35572da3ac741c4603d2c
ani2689 <ojno7777@gmail.com>
2024-03-13 21:08:53 +0900

    :sparkles: attendance controller

---
+++ src/main/kotlin/com/msg/gcms/domain/attendance/presentation/AttendanceController.kt
@@ -0,0 +1,24 @@
+package com.msg.gcms.domain.attendance.presentation
+
+import com.msg.gcms.domain.attendance.presentation.data.request.CreateScheduleRequestDto
+import com.msg.gcms.domain.attendance.service.CreateScheduleService
+import com.msg.gcms.domain.attendance.util.ScheduleConverter
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/attend")
+class AttendanceController(
+    private val createScheduleService: CreateScheduleService,
+    private val scheduleConverter: ScheduleConverter
+) {
+    @PostMapping("/{club_id}/club")
+    fun createSchedule(
+        @PathVariable("club_id") clubId: Long,
+        @RequestBody @Valid createScheduleRequestDto: CreateScheduleRequestDto
+    ): ResponseEntity<Unit> =
+        scheduleConverter.toDto(createScheduleRequestDto)
+            .let { createScheduleService.execute(clubId, it) }
+            .let { ResponseEntity.noContent().build() }
+}
