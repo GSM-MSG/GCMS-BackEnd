6c8f53841c7eb505557d1dc04e358eaa8f65491c
ani2689 <ojno7777@gmail.com>
2024-03-14 03:49:35 +0900

    :sparkles: request to dto

--- src/main/kotlin/com/msg/gcms/domain/attendance/util/AttendanceConverter.kt
+++ src/main/kotlin/com/msg/gcms/domain/attendance/util/AttendanceConverter.kt
@@ -3,12 +3,16 @@ package com.msg.gcms.domain.attendance.util
 import com.msg.gcms.domain.attendance.domain.entity.Attendance
 import com.msg.gcms.domain.attendance.domain.entity.Schedule
 import com.msg.gcms.domain.attendance.domain.enums.AttendanceStatus
+import com.msg.gcms.domain.attendance.presentation.data.dto.AttendanceStatusDto
+import com.msg.gcms.domain.attendance.presentation.data.request.UpdateAttendanceStatusRequestDto
 import com.msg.gcms.domain.user.domain.entity.User
 
 interface AttendanceConverter {
     fun toEntity(
+        id: Long = 0L,
         attendanceStatus: AttendanceStatus = AttendanceStatus.NOT_ATTENDED,
         user: User,
         schedule: Schedule
     ): Attendance
+    fun toDto(attendanceStatusDto: UpdateAttendanceStatusRequestDto): AttendanceStatusDto
 }
