963a5f7b5e5f6620dd8cf61027087519791b9836
ani2689 <ojno7777@gmail.com>
2024-03-14 03:51:54 +0900

    :sparkles: update attendance status mapping

--- src/main/kotlin/com/msg/gcms/domain/attendance/presentation/AttendanceController.kt
+++ src/main/kotlin/com/msg/gcms/domain/attendance/presentation/AttendanceController.kt
@@ -1,18 +1,24 @@
 package com.msg.gcms.domain.attendance.presentation
 
 import com.msg.gcms.domain.attendance.presentation.data.request.CreateScheduleRequestDto
+import com.msg.gcms.domain.attendance.presentation.data.request.UpdateAttendanceStatusRequestDto
 import com.msg.gcms.domain.attendance.service.CreateScheduleService
+import com.msg.gcms.domain.attendance.service.UpdateAttendanceStatusService
+import com.msg.gcms.domain.attendance.util.AttendanceConverter
 import com.msg.gcms.domain.attendance.util.ScheduleConverter
 import com.msg.gcms.global.annotation.RequestController
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.*
 import javax.validation.Valid
 
 @RequestController("/attend")
 class AttendanceController(
     private val createScheduleService: CreateScheduleService,
-    private val scheduleConverter: ScheduleConverter
+    private val updateAttendanceStatusService: UpdateAttendanceStatusService,
+    private val scheduleConverter: ScheduleConverter,
+    private val attendanceConverter: AttendanceConverter
 ) {
     @PostMapping("/{club_id}/club")
     fun createSchedule(
@@ -22,4 +28,13 @@ class AttendanceController(
         scheduleConverter.toDto(createScheduleRequestDto)
             .let { createScheduleService.execute(clubId, it) }
             .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+
+    @PatchMapping("/{user_id}")
+    fun updateAttendanceStatus(
+        @PathVariable("user_id") userId: UUID,
+        @RequestBody @Valid updateAttendanceStatusRequestDto: UpdateAttendanceStatusRequestDto
+    ): ResponseEntity<Unit> =
+        attendanceConverter.toDto(updateAttendanceStatusRequestDto)
+            .let { updateAttendanceStatusService.execute(userId, it) }
+            .let { ResponseEntity.noContent().build() }
 }
