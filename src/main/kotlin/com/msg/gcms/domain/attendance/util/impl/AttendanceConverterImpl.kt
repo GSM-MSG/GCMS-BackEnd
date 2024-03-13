e66a36dbe454be1a35cb73510079dc2f3f6aa888
ani2689 <ojno7777@gmail.com>
2024-03-13 21:08:02 +0900

    :sparkles: attendance converter

---
+++ src/main/kotlin/com/msg/gcms/domain/attendance/util/impl/AttendanceConverterImpl.kt
@@ -0,0 +1,20 @@
+package com.msg.gcms.domain.attendance.util.impl
+
+import com.msg.gcms.domain.attendance.domain.entity.Attendance
+import com.msg.gcms.domain.attendance.domain.entity.Schedule
+import com.msg.gcms.domain.attendance.domain.enums.AttendanceStatus
+import com.msg.gcms.domain.attendance.util.AttendanceConverter
+import com.msg.gcms.domain.user.domain.entity.User
+import org.springframework.stereotype.Component
+
+@Component
+class AttendanceConverterImpl : AttendanceConverter {
+    override fun toEntity(
+        attendanceStatus: AttendanceStatus,
+        user: User, schedule: Schedule
+    ): Attendance = Attendance(
+        attendanceStatus = attendanceStatus,
+        user = user,
+        schedule = schedule
+    )
+}
