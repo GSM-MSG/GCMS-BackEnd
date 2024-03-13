a589705c33d887a2e3eaf9e4f3f3acf1eca3cbe7
ani2689 <ojno7777@gmail.com>
2024-03-13 21:07:00 +0900

    :sparkles: schedule dto

---
+++ src/main/kotlin/com/msg/gcms/domain/attendance/presentation/data/request/CreateScheduleRequestDto.kt
@@ -0,0 +1,18 @@
+package com.msg.gcms.domain.attendance.presentation.data.request
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import java.time.LocalDate
+import java.time.LocalTime
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
+data class CreateScheduleRequestDto (
+    @NotBlank
+    val name: String,
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    val date: LocalDate,
+
+    @field:NotNull
+    val periods: List<LocalTime>
+)
