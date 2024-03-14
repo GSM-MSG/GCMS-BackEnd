82f2adba3f05a986079970ac78a67ce2f885022c
KimTaeO <taeo0328@gmail.com>
2024-03-14 09:57:43 +0900

    add :: find by schedule entitygraph

--- src/main/kotlin/com/msg/gcms/domain/attendance/repository/AttendanceRepository.kt
+++ src/main/kotlin/com/msg/gcms/domain/attendance/repository/AttendanceRepository.kt
@@ -1,6 +1,11 @@
 package com.msg.gcms.domain.attendance.repository
 
 import com.msg.gcms.domain.attendance.domain.entity.Attendance
+import com.msg.gcms.domain.attendance.domain.entity.Schedule
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.repository.CrudRepository
 
-interface AttendanceRepository : CrudRepository<Attendance, Long>
+interface AttendanceRepository : CrudRepository<Attendance, Long> {
+    @EntityGraph("user")
+    fun findAllBySchedule(schedule: Schedule): List<Attendance>
+}
