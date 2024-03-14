e96054aa2c3ef1db2686e034a862467ccea46b13
KimTaeO <taeo0328@gmail.com>
2024-03-14 09:51:11 +0900

    create :: data transfer layer

---
+++ src/main/kotlin/com/msg/gcms/domain/attendance/presentation/data/dto/UserAttendanceStatusDto.kt
@@ -0,0 +1,13 @@
+package com.msg.gcms.domain.attendance.presentation.data.dto
+
+import com.msg.gcms.domain.attendance.domain.enums.AttendanceStatus
+import java.util.*
+
+data class UserAttendanceStatusDto(
+    val id: UUID,
+    val name: String,
+    val grade: Int,
+    val classNum: Int,
+    val number: Int,
+    val attendanceStatus: AttendanceStatus
+)
