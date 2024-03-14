9500a89b94b6e1227f0a98976eaded2b03c17a14
김태오 <103710151+KimTaeO@users.noreply.github.com>
2024-03-14 15:24:53 +0900

    Update src/main/kotlin/com/msg/gcms/domain/attendance/exception/ScheduleNotFoundException.kt
    Co-authored-by: 노현주 <106813267+ani2689@users.noreply.github.com>

--- src/main/kotlin/com/msg/gcms/domain/attendance/exception/ScheduleNotFoundException.kt
+++ src/main/kotlin/com/msg/gcms/domain/attendance/exception/ScheduleNotFoundException.kt
@@ -3,5 +3,4 @@ package com.msg.gcms.domain.attendance.exception
 import com.msg.gcms.global.exception.ErrorCode
 import com.msg.gcms.global.exception.exceptions.BasicException
 
-class ScheduleNotFoundException : BasicException(ErrorCode.SCHEDULE_NOT_FOUND) {
-}
+class ScheduleNotFoundException : BasicException(ErrorCode.SCHEDULE_NOT_FOUND)
