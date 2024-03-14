d73cc5ee97385b0d84ab80e7bfb6caba067f9ff1
KimTaeO <taeo0328@gmail.com>
2024-03-14 16:17:19 +0900

    add :: use custom transaction annotation

--- src/main/kotlin/com/msg/gcms/domain/attendance/service/impl/QueryCurrentAttendConditionServiceImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/attendance/service/impl/QueryCurrentAttendConditionServiceImpl.kt
@@ -2,23 +2,21 @@ package com.msg.gcms.domain.attendance.service.impl
 
 import com.msg.gcms.domain.attendance.exception.ScheduleNotFoundException
 import com.msg.gcms.domain.attendance.presentation.data.dto.SearchScheduleDto
+import com.msg.gcms.domain.attendance.presentation.data.dto.UserAttendanceStatusListDto
 import com.msg.gcms.domain.attendance.repository.AttendanceRepository
 import com.msg.gcms.domain.attendance.repository.ScheduleRepository
 import com.msg.gcms.domain.attendance.service.QueryCurrentAttendConditionService
 import com.msg.gcms.domain.attendance.util.AttendanceConverter
+import com.msg.gcms.domain.auth.domain.Role
 import com.msg.gcms.domain.club.domain.repository.ClubRepository
 import com.msg.gcms.domain.club.exception.ClubNotFoundException
 import com.msg.gcms.domain.club.exception.HeadNotSameException
 import com.msg.gcms.domain.user.domain.entity.User
-import com.msg.gcms.domain.attendance.presentation.data.dto.UserAttendanceStatusListDto
-import com.msg.gcms.domain.auth.domain.Role
+import com.msg.gcms.global.annotation.ServiceWithReadOnlyTransaction
 import com.msg.gcms.global.util.UserUtil
 import org.springframework.data.repository.findByIdOrNull
-import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
-@Service
-@Transactional(rollbackFor = [Exception::class], readOnly = true)
+@ServiceWithReadOnlyTransaction
 class QueryCurrentAttendConditionServiceImpl(
     private val userUtil: UserUtil,
     private val clubRepository: ClubRepository,
