bf236867190672687d7392dad730b193e171fc9f
ani2689 <ojno7777@gmail.com>
2024-03-14 14:44:22 +0900

    :sparkles: update attendance dto list dto

--- src/main/kotlin/com/msg/gcms/domain/attendance/util/AttendanceConverter.kt
+++ src/main/kotlin/com/msg/gcms/domain/attendance/util/AttendanceConverter.kt
@@ -4,6 +4,8 @@ import com.msg.gcms.domain.attendance.domain.entity.Attendance
 import com.msg.gcms.domain.attendance.domain.entity.Schedule
 import com.msg.gcms.domain.attendance.domain.enums.AttendanceStatus
 import com.msg.gcms.domain.attendance.presentation.data.dto.AttendanceDto
+import com.msg.gcms.domain.attendance.presentation.data.dto.AttendanceDto.AttendanceListDto
+import com.msg.gcms.domain.attendance.presentation.data.request.UpdateAttendanceStatusBatchRequestDto
 import com.msg.gcms.domain.attendance.presentation.data.request.UpdateAttendanceStatusRequestDto
 import com.msg.gcms.domain.user.domain.entity.User
 import java.util.UUID
@@ -16,4 +18,5 @@ interface AttendanceConverter {
         schedule: Schedule
     ): Attendance
     fun toDto(attendanceStatusDto: UpdateAttendanceStatusRequestDto, userId: UUID): AttendanceDto
+    fun toDto(attendanceStatusDto: UpdateAttendanceStatusBatchRequestDto): AttendanceListDto
 }
--- src/main/kotlin/com/msg/gcms/domain/attendance/util/impl/AttendanceConverterImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/attendance/util/impl/AttendanceConverterImpl.kt
@@ -4,6 +4,8 @@ import com.msg.gcms.domain.attendance.domain.entity.Attendance
 import com.msg.gcms.domain.attendance.domain.entity.Schedule
 import com.msg.gcms.domain.attendance.domain.enums.AttendanceStatus
 import com.msg.gcms.domain.attendance.presentation.data.dto.AttendanceDto
+import com.msg.gcms.domain.attendance.presentation.data.dto.AttendanceDto.AttendanceListDto
+import com.msg.gcms.domain.attendance.presentation.data.request.UpdateAttendanceStatusBatchRequestDto
 import com.msg.gcms.domain.attendance.presentation.data.request.UpdateAttendanceStatusRequestDto
 import com.msg.gcms.domain.attendance.util.AttendanceConverter
 import com.msg.gcms.domain.user.domain.entity.User
@@ -29,4 +31,12 @@ class AttendanceConverterImpl : AttendanceConverter {
         attendanceStatus = attendanceStatusDto.attendanceStatus,
         userId = userId
     )
+
+    override fun toDto(attendanceStatusDto: UpdateAttendanceStatusBatchRequestDto): AttendanceListDto = attendanceStatusDto.userIds.map {
+        AttendanceDto(
+            scheduleId = attendanceStatusDto.scheduleId,
+            attendanceStatus = attendanceStatusDto.attendanceStatus,
+            userId = it
+        )
+    }.let { AttendanceListDto(it) }
 }
