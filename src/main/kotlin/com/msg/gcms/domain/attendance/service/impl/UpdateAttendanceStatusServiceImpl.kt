3586296e3c01f43d05797d7af9245d80fc1c208f
ani2689 <ojno7777@gmail.com>
2024-03-20 17:03:01 +0900

    :recycle: period 변경사항 서비스에 적용

--- src/main/kotlin/com/msg/gcms/domain/attendance/service/impl/UpdateAttendanceStatusServiceImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/attendance/service/impl/UpdateAttendanceStatusServiceImpl.kt
@@ -1,48 +1,43 @@
 package com.msg.gcms.domain.attendance.service.impl
 
 import com.msg.gcms.domain.attendance.exception.AttendanceNotFoundException
-import com.msg.gcms.domain.attendance.exception.ScheduleNotFoundException
 import com.msg.gcms.domain.attendance.presentation.data.dto.AttendanceDto
 import com.msg.gcms.domain.attendance.repository.AttendanceRepository
-import com.msg.gcms.domain.attendance.repository.ScheduleRepository
 import com.msg.gcms.domain.attendance.service.UpdateAttendanceStatusService
 import com.msg.gcms.domain.attendance.util.AttendanceConverter
 import com.msg.gcms.domain.auth.domain.Role
+import com.msg.gcms.domain.club.domain.repository.ClubRepository
+import com.msg.gcms.domain.club.exception.ClubNotFoundException
 import com.msg.gcms.domain.club.exception.HeadNotSameException
-import com.msg.gcms.domain.user.domain.repository.UserRepository
-import com.msg.gcms.domain.user.exception.UserNotFoundException
 import com.msg.gcms.global.annotation.ServiceWithTransaction
 import com.msg.gcms.global.util.UserUtil
 import org.springframework.data.repository.findByIdOrNull
 
 @ServiceWithTransaction
 class UpdateAttendanceStatusServiceImpl(
-    private val userRepository: UserRepository,
-    private val scheduleRepository: ScheduleRepository,
+    private val clubRepository: ClubRepository,
     private val attendanceRepository: AttendanceRepository,
     private val attendanceConverter: AttendanceConverter,
     private val userUtil: UserUtil
 ) : UpdateAttendanceStatusService {
-    override fun execute(dto: AttendanceDto) {
+    override fun execute(dto: AttendanceDto, clubId: Long) {
         val currentUser = userUtil.fetchCurrentUser()
 
-        val schedule = scheduleRepository.findByIdOrNull(dto.scheduleId)
-            ?: throw ScheduleNotFoundException()
+        val club = clubRepository.findByIdOrNull(clubId)
+            ?: throw ClubNotFoundException()
 
-        if (schedule.club.user != currentUser && currentUser.roles[0] != Role.ROLE_ADMIN)
+        if (club.user != currentUser && currentUser.roles[0] != Role.ROLE_ADMIN)
             throw HeadNotSameException()
 
-        val user = userRepository.findByIdOrNull(dto.userId)
-            ?: throw UserNotFoundException()
-
-        val attendance = attendanceRepository.findByUserAndSchedule(user, schedule)
+        val attendance = attendanceRepository.findByIdOrNull(dto.id)
             ?: throw AttendanceNotFoundException()
 
         attendanceConverter.toEntity(
             id = attendance.id,
             attendanceStatus = dto.attendanceStatus,
             user = attendance.user,
-            schedule = attendance.schedule
+            schedule = attendance.schedule,
+            period = attendance.period
         ).let { attendanceRepository.save(it) }
     }
 }
