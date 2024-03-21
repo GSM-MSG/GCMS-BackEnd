735d6f6c4101fc40ca4b4a4ad14639e9fd24b265
KimTaeO <taeo0328@gmail.com>
2024-03-22 00:54:21 +0900

    add :: find attendance all by date

--- src/main/kotlin/com/msg/gcms/domain/attendance/repository/ScheduleRepository.kt
+++ src/main/kotlin/com/msg/gcms/domain/attendance/repository/ScheduleRepository.kt
@@ -2,5 +2,8 @@ package com.msg.gcms.domain.attendance.repository
 
 import com.msg.gcms.domain.attendance.domain.entity.Schedule
 import org.springframework.data.repository.CrudRepository
+import java.time.LocalDate
 
-interface ScheduleRepository : CrudRepository<Schedule, Long>, CustomScheduleRepository
+interface ScheduleRepository : CrudRepository<Schedule, Long>, CustomScheduleRepository {
+    fun findAllByDate(localDate: LocalDate): List<Schedule>
+}
