93d103f4f6c1f152ebc28ae97913d4f63f1f9371
ani2689 <ojno7777@gmail.com>
2024-03-21 15:13:13 +0900

    :sparkles: 출석 상태 enum에 기호와 설명 추가

--- src/main/kotlin/com/msg/gcms/domain/attendance/domain/enums/AttendanceStatus.kt
+++ src/main/kotlin/com/msg/gcms/domain/attendance/domain/enums/AttendanceStatus.kt
@@ -1,5 +1,13 @@
 package com.msg.gcms.domain.attendance.domain.enums
 
-enum class AttendanceStatus {
-    NOT_ATTENDED, ATTENDED, LATE, REASONABLE_ABSENT, ABSENT;
+enum class AttendanceStatus(
+    val description: String,
+    val sign: String
+) {
+    NOT_ATTENDED("미출석","-"),
+    ATTENDED("출석","O"),
+    LATE("지각","△"),
+    REASONABLE_ABSENT("인정결석","."),
+    ABSENT("무단결석","X")
+    ;
 }
