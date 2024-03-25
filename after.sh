7347d48ba922ddb70341f65325508dbecc05be88
shk0625 <tjfgml1238@gmail.com>
2024-03-26 00:10:54 +0900

    update:: appspec.yml 수정

--- after.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-PROJECT_PATH=/home/ec2-user/deploy
-cd ${PROJECT_PATH}
-docker-compose up -d
--- deploy-before.sh
+++
@@ -1,4 +0,0 @@
-if [ -d /home/ec2-user/deploy ]; then
-    sudo rm -rf /home/ec2-user/deploy/
-fi
-sudo mkdir -vp /home/ec2-user/deploy/
