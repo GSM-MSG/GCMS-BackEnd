b08eca1d739cd313d1b3d6612f530c93dd72bb63
taeyeon <tyeon42051@naver.com>
2024-04-02 17:15:19 +0900

    create :: Notice Not Found Exception

--- src/main/kotlin/com/msg/gcms/global/exception/ErrorCode.kt
+++ src/main/kotlin/com/msg/gcms/global/exception/ErrorCode.kt
@@ -37,6 +37,7 @@ enum class ErrorCode(
     DEVICE_TOKEN_NOT_FOUND("디바이스 토큰을 찾을 수 없음", 404),
     SCHEDULE_NOT_FOUND("출석부를 찾을 수 없음", 404),
     ATTENDANCE_NOT_FOUND("출석 현황을 찾을 수 없음", 404),
+    NOTICE_NOT_FOUND("해당 공지를 찾을 수 없음", 404),
 
     ALREADY_CLUB_EXIST("해당 동아리가 이미 존재함", 409),
     OPENING_APPLICATION_ALREADY_EXIST("이미 개설신청서를 제출하였습니다.", 409),
