8a10234465f4247dd5f6c3ae14c073bbecf88579
taeyeon <tyeon42051@naver.com>
2024-04-02 17:15:57 +0900

    add :: 공지 사항 삭제 구현

---
+++ src/main/kotlin/com/msg/gcms/domain/notice/service/impl/DeleteNoticeServiceImpl.kt
@@ -0,0 +1,29 @@
+package com.msg.gcms.domain.notice.service.impl
+
+import com.msg.gcms.domain.auth.domain.Role
+import com.msg.gcms.domain.club.exception.HeadNotSameException
+import com.msg.gcms.domain.notice.domain.repository.NoticeRepository
+import com.msg.gcms.domain.notice.exception.NoticeNotFoundException
+import com.msg.gcms.domain.notice.service.DeleteNoticeService
+import com.msg.gcms.global.annotation.ServiceWithTransaction
+import com.msg.gcms.global.util.UserUtil
+import org.springframework.data.repository.findByIdOrNull
+
+@ServiceWithTransaction
+class DeleteNoticeServiceImpl(
+        private val userUtil: UserUtil,
+        private val noticeRepository: NoticeRepository
+): DeleteNoticeService {
+    override fun execute(id: Long) {
+        val user = userUtil.fetchCurrentUser()
+
+        if (user.roles.contains(Role.ROLE_STUDENT)) {
+            throw HeadNotSameException()
+        }
+
+        val notice = noticeRepository.findByIdOrNull(id)
+                ?: throw NoticeNotFoundException()
+
+        noticeRepository.delete(notice)
+    }
+}
