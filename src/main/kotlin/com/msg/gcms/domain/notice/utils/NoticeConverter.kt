d8f188612b8a71f1b11ce2d091bfe2badc636a98
taeyeon <tyeon42051@naver.com>
2024-04-03 16:57:05 +0900

    add :: converter 추가

--- src/main/kotlin/com/msg/gcms/domain/notice/utils/NoticeConverter.kt
+++ src/main/kotlin/com/msg/gcms/domain/notice/utils/NoticeConverter.kt
@@ -4,10 +4,18 @@ import com.msg.gcms.domain.club.domain.entity.Club
 import com.msg.gcms.domain.notice.domain.entity.Notice
 import com.msg.gcms.domain.notice.presentation.data.dto.NoticeDto
 import com.msg.gcms.domain.notice.presentation.data.request.CreateNoticeRequestDto
+import com.msg.gcms.domain.notice.presentation.data.response.NoticeListDto
+import com.msg.gcms.domain.notice.presentation.data.response.NoticeListResponseDto
 import com.msg.gcms.domain.user.domain.entity.User
 
 interface NoticeConverter {
     fun toDto(requestDto: CreateNoticeRequestDto): NoticeDto
 
     fun toEntity(dto: NoticeDto, user: User, club: Club): Notice
+
+    fun toDto(entity: Notice): NoticeListDto.NoticeResponseDto
+
+    fun toListDto(dto: List<NoticeListDto.NoticeResponseDto>): NoticeListDto
+
+    fun toResponse(dto: NoticeListDto): NoticeListResponseDto
 }
