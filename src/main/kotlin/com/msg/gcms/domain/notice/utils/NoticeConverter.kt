d46bdd27b732e2997d68330516b23210c63db0ef
KimTaeO <taeo0328@gmail.com>
2024-04-03 18:03:40 +0900

    add :: find notice detail converter

--- src/main/kotlin/com/msg/gcms/domain/notice/utils/NoticeConverter.kt
+++ src/main/kotlin/com/msg/gcms/domain/notice/utils/NoticeConverter.kt
@@ -4,10 +4,11 @@ import com.msg.gcms.domain.club.domain.entity.Club
 import com.msg.gcms.domain.notice.domain.entity.Notice
 import com.msg.gcms.domain.notice.presentation.data.dto.NoticeDto
 import com.msg.gcms.domain.notice.presentation.data.request.CreateNoticeRequestDto
+import com.msg.gcms.domain.notice.presentation.data.response.FindNoticeDetailResponseDto
 import com.msg.gcms.domain.user.domain.entity.User
 
 interface NoticeConverter {
     fun toDto(requestDto: CreateNoticeRequestDto): NoticeDto
-
+    fun toDto(notice: Notice): FindNoticeDetailResponseDto
     fun toEntity(dto: NoticeDto, user: User, club: Club): Notice
 }
--- src/main/kotlin/com/msg/gcms/domain/notice/utils/impl/NoticeConverterImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/notice/utils/impl/NoticeConverterImpl.kt
@@ -4,6 +4,7 @@ import com.msg.gcms.domain.club.domain.entity.Club
 import com.msg.gcms.domain.notice.domain.entity.Notice
 import com.msg.gcms.domain.notice.presentation.data.dto.NoticeDto
 import com.msg.gcms.domain.notice.presentation.data.request.CreateNoticeRequestDto
+import com.msg.gcms.domain.notice.presentation.data.response.FindNoticeDetailResponseDto
 import com.msg.gcms.domain.notice.utils.NoticeConverter
 import com.msg.gcms.domain.user.domain.entity.User
 import org.springframework.stereotype.Component
@@ -13,6 +14,14 @@ class NoticeConverterImpl : NoticeConverter {
     override fun toDto(requestDto: CreateNoticeRequestDto): NoticeDto = NoticeDto(
         title = requestDto.title,
         content = requestDto.content
+    )
+
+    override fun toDto(notice: Notice): FindNoticeDetailResponseDto = FindNoticeDetailResponseDto(
+        title = notice.title,
+        content = notice.content,
+        username = notice.user.nickname,
+        userProfileImg = notice.user.profileImg,
+        createdAt = notice.createdAt
     )
 
     override fun toEntity(dto: NoticeDto, user: User, club: Club): Notice = Notice(
