41994d323977f90a7d840598f9a65f650c203b66
taeyeon <tyeon42051@naver.com>
2024-04-03 16:57:21 +0900

    add :: 공지사항 리스트 불러오기

---
+++ src/main/kotlin/com/msg/gcms/domain/notice/service/impl/NoticeListServiceImpl.kt
@@ -0,0 +1,38 @@
+package com.msg.gcms.domain.notice.service.impl
+
+import com.msg.gcms.domain.auth.domain.Role
+import com.msg.gcms.domain.club.domain.repository.ClubRepository
+import com.msg.gcms.domain.club.exception.ClubNotFoundException
+import com.msg.gcms.domain.club.exception.NotClubMemberException
+import com.msg.gcms.domain.notice.domain.repository.NoticeRepository
+import com.msg.gcms.domain.notice.presentation.data.response.NoticeListDto
+import com.msg.gcms.domain.notice.service.NoticeListService
+import com.msg.gcms.domain.notice.utils.NoticeConverter
+import com.msg.gcms.global.annotation.ServiceWithTransaction
+import com.msg.gcms.global.util.UserUtil
+import org.springframework.data.repository.findByIdOrNull
+
+@ServiceWithTransaction
+class NoticeListServiceImpl(
+        private val noticeRepository: NoticeRepository,
+        private val clubRepository: ClubRepository,
+        private val noticeConverter: NoticeConverter,
+        private val userUtil: UserUtil
+) : NoticeListService {
+    override fun execute(clubId: Long): NoticeListDto {
+        val club = clubRepository.findByIdOrNull(clubId)
+                ?: throw ClubNotFoundException()
+        val user = userUtil.fetchCurrentUser()
+
+        if (user.roles.contains(Role.ROLE_STUDENT)){
+            if (!user.club.contains(club)) {
+                throw NotClubMemberException()
+            }
+        }
+
+        val notices = noticeRepository.findByClub(club)
+                .map { noticeConverter.toDto(it) }
+
+        return noticeConverter.toListDto(notices)
+    }
+}
