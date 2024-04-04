5c8521fe79d7f439e2aab2ab90b5b56c54780a81
KimTaeO <taeo0328@gmail.com>
2024-04-04 17:03:31 +0900

    add :: update notice dto converter

--- src/main/kotlin/com/msg/gcms/domain/notice/utils/NoticeConverter.kt
+++ src/main/kotlin/com/msg/gcms/domain/notice/utils/NoticeConverter.kt
@@ -3,7 +3,9 @@ package com.msg.gcms.domain.notice.utils
 import com.msg.gcms.domain.club.domain.entity.Club
 import com.msg.gcms.domain.notice.domain.entity.Notice
 import com.msg.gcms.domain.notice.presentation.data.dto.NoticeDto
+import com.msg.gcms.domain.notice.presentation.data.dto.UpdateNoticeDto
 import com.msg.gcms.domain.notice.presentation.data.request.CreateNoticeRequestDto
+import com.msg.gcms.domain.notice.presentation.data.request.UpdateNoticeRequestDto
 import com.msg.gcms.domain.notice.presentation.data.response.FindNoticeDetailResponseDto
 import com.msg.gcms.domain.notice.presentation.data.response.NoticeListDto
 import com.msg.gcms.domain.notice.presentation.data.response.NoticeListResponseDto
@@ -11,12 +13,15 @@ import com.msg.gcms.domain.user.domain.entity.User
 
 interface NoticeConverter {
     fun toDto(requestDto: CreateNoticeRequestDto): NoticeDto
-    fun toResponse(notice: Notice): FindNoticeDetailResponseDto
     fun toEntity(dto: NoticeDto, user: User, club: Club): Notice
 
     fun toDto(entity: Notice): NoticeListDto.NoticeResponseDto
 
+    fun toDto(requestDto: UpdateNoticeRequestDto): UpdateNoticeDto
+
     fun toListDto(dto: List<NoticeListDto.NoticeResponseDto>): NoticeListDto
 
     fun toResponse(dto: NoticeListDto): NoticeListResponseDto
+
+    fun toResponse(notice: Notice): FindNoticeDetailResponseDto
 }
--- src/main/kotlin/com/msg/gcms/domain/notice/utils/impl/NoticeConverterImpl.kt
+++ src/main/kotlin/com/msg/gcms/domain/notice/utils/impl/NoticeConverterImpl.kt
@@ -3,7 +3,9 @@ package com.msg.gcms.domain.notice.utils.impl
 import com.msg.gcms.domain.club.domain.entity.Club
 import com.msg.gcms.domain.notice.domain.entity.Notice
 import com.msg.gcms.domain.notice.presentation.data.dto.NoticeDto
+import com.msg.gcms.domain.notice.presentation.data.dto.UpdateNoticeDto
 import com.msg.gcms.domain.notice.presentation.data.request.CreateNoticeRequestDto
+import com.msg.gcms.domain.notice.presentation.data.request.UpdateNoticeRequestDto
 import com.msg.gcms.domain.notice.presentation.data.response.FindNoticeDetailResponseDto
 import com.msg.gcms.domain.notice.presentation.data.response.NoticeListDto
 import com.msg.gcms.domain.notice.presentation.data.response.NoticeListResponseDto
@@ -18,20 +20,18 @@ class NoticeConverterImpl : NoticeConverter {
         content = requestDto.content
     )
 
-    override fun toResponse(notice: Notice): FindNoticeDetailResponseDto = FindNoticeDetailResponseDto(
-        title = notice.title,
-        content = notice.content,
-        username = notice.user.nickname,
-        userProfileImg = notice.user.profileImg,
-        createdAt = notice.createdAt
-    )
-
     override fun toDto(entity: Notice): NoticeListDto.NoticeResponseDto = NoticeListDto.NoticeResponseDto(
         id = entity.id,
         title = entity.title,
         username = entity.user.nickname,
         createdAt = entity.createdAt
     )
+
+    override fun toDto(requestDto: UpdateNoticeRequestDto): UpdateNoticeDto =
+        UpdateNoticeDto(
+            title = requestDto.title,
+            content = requestDto.content
+        )
 
     override fun toEntity(dto: NoticeDto, user: User, club: Club): Notice = Notice(
         id = dto.id,
@@ -42,6 +42,7 @@ class NoticeConverterImpl : NoticeConverter {
         club = club
     )
 
+
     override fun toListDto(dto: List<NoticeListDto.NoticeResponseDto>): NoticeListDto =
          NoticeListDto(
             notices = dto
@@ -51,4 +52,13 @@ class NoticeConverterImpl : NoticeConverter {
             NoticeListResponseDto(
                     notices = dto.notices
             )
+
+    override fun toResponse(notice: Notice): FindNoticeDetailResponseDto = FindNoticeDetailResponseDto(
+        title = notice.title,
+        content = notice.content,
+        username = notice.user.nickname,
+        userProfileImg = notice.user.profileImg,
+        createdAt = notice.createdAt
+    )
+
 }
