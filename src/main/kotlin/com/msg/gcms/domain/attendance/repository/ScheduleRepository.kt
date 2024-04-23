8c7165916bcaafbf86753d6c7212da3d3d9343ec
KimTaeO <taeo0328@gmail.com>
2024-04-23 23:40:05 +0900

    update :: club entity save logic

--- src/main/kotlin/com/msg/gcms/domain/attendance/repository/ScheduleRepository.kt
+++ src/main/kotlin/com/msg/gcms/domain/attendance/repository/ScheduleRepository.kt
@@ -1,9 +1,11 @@
 package com.msg.gcms.domain.attendance.repository
 
 import com.msg.gcms.domain.attendance.domain.entity.Schedule
+import com.msg.gcms.domain.club.domain.entity.Club
 import org.springframework.data.repository.CrudRepository
 import java.time.LocalDate
 
 interface ScheduleRepository : CrudRepository<Schedule, Long>, CustomScheduleRepository {
     fun findAllByDate(localDate: LocalDate): List<Schedule>
+    fun findByClubAndDate(club: Club, localDate: LocalDate): Schedule?
 }
