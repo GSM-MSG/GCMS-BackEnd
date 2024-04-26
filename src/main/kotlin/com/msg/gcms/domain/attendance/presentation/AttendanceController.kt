ccc2fa7ea557c141b335252b19887c8d73542e19
태연 <132069501+ta2ye0n@users.noreply.github.com>
2024-04-26 14:10:44 +0900

    Update src/main/kotlin/com/msg/gcms/domain/attendance/presentation/AttendanceController.kt
    Co-authored-by: 노현주 <106813267+ani2689@users.noreply.github.com>

--- src/main/kotlin/com/msg/gcms/domain/attendance/presentation/AttendanceController.kt
+++ src/main/kotlin/com/msg/gcms/domain/attendance/presentation/AttendanceController.kt
@@ -75,7 +75,7 @@ class AttendanceController(
     }
 
     @GetMapping("/{club_id}/my")
-    fun findAttendSelfCheck(@PathVariable("club_id") id: Long): ResponseEntity<AttendSelfCheckResponseDto> =
+    fun findAttendSelfCheck(@PathVariable("club_id") clubId: Long): ResponseEntity<AttendSelfCheckResponseDto> =
         findAttendSelfCheckService.execute(id)
             .let { ResponseEntity.ok(it) }
 }
