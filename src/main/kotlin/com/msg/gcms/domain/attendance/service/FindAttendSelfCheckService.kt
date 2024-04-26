056775c32a431ec54df58685e95a2b9a8c9e7c1f
태연 <132069501+ta2ye0n@users.noreply.github.com>
2024-04-26 14:10:21 +0900

    Update src/main/kotlin/com/msg/gcms/domain/attendance/service/FindAttendSelfCheckService.kt
    Co-authored-by: 노현주 <106813267+ani2689@users.noreply.github.com>

--- src/main/kotlin/com/msg/gcms/domain/attendance/service/FindAttendSelfCheckService.kt
+++ src/main/kotlin/com/msg/gcms/domain/attendance/service/FindAttendSelfCheckService.kt
@@ -3,5 +3,5 @@ package com.msg.gcms.domain.attendance.service
 import com.msg.gcms.domain.attendance.presentation.data.response.AttendSelfCheckResponseDto
 
 interface FindAttendSelfCheckService {
-    fun execute(id : Long) : AttendSelfCheckResponseDto
+    fun execute(id: Long) : AttendSelfCheckResponseDto
 }
