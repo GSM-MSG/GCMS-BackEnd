cb8dfa76ce39b92db8eb798a76e863c8bef50502
taeyeon <tyeon42051@naver.com>
2024-04-26 10:53:58 +0900

    add :: FindAttendSelfCheckServiceImpl

---
+++ src/main/kotlin/com/msg/gcms/domain/attendance/service/impl/FindAttendSelfCheckServiceImpl.kt
@@ -0,0 +1,48 @@
+package com.msg.gcms.domain.attendance.service.impl
+
+import com.msg.gcms.domain.attendance.domain.enums.Period
+import com.msg.gcms.domain.attendance.presentation.data.response.AttendSelfCheckResponseDto
+import com.msg.gcms.domain.attendance.repository.AttendanceRepository
+import com.msg.gcms.domain.attendance.service.FindAttendSelfCheckService
+import com.msg.gcms.domain.attendance.util.AttendanceConverter
+import com.msg.gcms.domain.club.domain.repository.ClubRepository
+import com.msg.gcms.domain.club.exception.ClubNotFoundException
+import com.msg.gcms.domain.club.exception.NotClubMemberException
+import com.msg.gcms.global.annotation.ServiceWithReadOnlyTransaction
+import com.msg.gcms.global.util.UserUtil
+import org.springframework.data.repository.findByIdOrNull
+
+@ServiceWithReadOnlyTransaction
+class FindAttendSelfCheckServiceImpl (
+        private val userUtil: UserUtil,
+        private val attendanceRepository: AttendanceRepository,
+        private val clubRepository: ClubRepository,
+        private val attendanceConverter: AttendanceConverter
+): FindAttendSelfCheckService {
+    override fun execute(id: Long): AttendSelfCheckResponseDto {
+        val user = userUtil.fetchCurrentUser()
+        val club = clubRepository.findByIdOrNull(id)
+                ?: throw ClubNotFoundException()
+
+        if (club.user != user) {
+            throw NotClubMemberException()
+        }
+
+        val period = getCurrentPeriod()
+        val attend = attendanceRepository.findByPeriodAndUser(period, user)
+
+        return attendanceConverter.toDto(attend)
+    }
+
+    fun getCurrentPeriod(): Period {
+        val currentTime = java.time.LocalTime.now()
+
+        Period.values().reversed().map {
+            if(currentTime.isAfter(it.time))
+                return it
+        }
+
+        return Period.ELEVENTH
+    }
+
+}
