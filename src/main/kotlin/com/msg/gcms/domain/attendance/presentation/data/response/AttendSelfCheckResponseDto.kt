208353cd99e19c950c337d2ac216d4708cfe3cfc
taeyeon <tyeon42051@naver.com>
2024-04-26 10:52:25 +0900

    add :: AttendSelfCheckResponseDto

---
+++ src/main/kotlin/com/msg/gcms/domain/attendance/presentation/data/response/AttendSelfCheckResponseDto.kt
@@ -0,0 +1,7 @@
+package com.msg.gcms.domain.attendance.presentation.data.response
+
+import com.msg.gcms.domain.attendance.domain.enums.AttendanceStatus
+
+data class AttendSelfCheckResponseDto(
+        val attendanceStatus: AttendanceStatus
+)
